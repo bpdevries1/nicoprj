a778016d85c185f668b961f6348713f2368d6a0b
vreeze42 <nico@ndv.nl>
2011-09-18 12:33:47 +0200

    Tcl: test lambda functions, some ideas.

---
+++ tryout/tcl-lambda/test-lambda.tcl
@@ -0,0 +1,12 @@
+set fn [function/lambda {args} {body}]
+
+keuze:
+set res [fn $args]
+set res [apply $fn $args]
+set res [$fn $args]
+
+#en hierna ook met map etc toepassen.
+
+#had al eens iets met lambda2proc en proc2lambda gedaan.
+
+
