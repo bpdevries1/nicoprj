4d1d301c7b04e1ed8cdf49bbf00c1f04119da5a8
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2011-12-19 14:30:53 +0100

    Show #items in TODO folders in Outlook.

---
+++ outlooktools/OutlookMoveRuby/showtodo.bat
@@ -0,0 +1,4 @@
+rem set bakfile=emailfolders-%_date.xml
+cd c:\nico\nicoprj\outlooktools\outlookmoveruby
+ruby showtodo.rb
+cd -
---
+++ outlooktools/OutlookMoveRuby/showtodo.rb
@@ -0,0 +1,42 @@
+require 'win32ole'
+require "FolderFinder.rb"
+require "EmailFolders.rb"
+require "FolderChooser.rb"
+require "LoggerFactory.rb"
+
+# alle OLE objecten hebben blijkbaar dezelfde class: WIN32OLE. Hier Enumerable aan
+# toevoegen, zodat bv find beschikbaar komt.
+class WIN32OLE
+	include Enumerable
+end
+
+class Main
+  
+  def initialize
+ 		@log = LoggerFactory.instance.get_logger "Main"
+		@log.level=INFO
+  end
+  
+  def run
+		puts "Outlook show number of items in TODO folders"
+		myApp = WIN32OLE::new("outlook.Application")
+		WIN32OLE.const_load(myApp, OutlookConst)
+
+		ns = myApp.GetNameSpace("MAPI")
+		#ns.Logon # uncomment for online usage
+		@ff = FolderFinder.new
+		@ff.set_namespace(ns)
+		
+		# @folder_chooser = FolderChooser.new
+		lst_todo = @ff.search_folders("TODO") # list of MailFolder
+		lst_todo.each {|fld| puts "#{fld.outlook_folder.items.count} -  #{fld.path}" if fld.outlook_folder.items.count > 0}
+  end
+
+end
+
+class OutlookConst
+end
+
+main = Main.new
+main.run
+
