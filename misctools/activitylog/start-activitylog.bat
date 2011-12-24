38f542208c3e9eacb4ca533888273d9403228747
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2011-12-24 12:18:43 +0100

    Use sqlite version on windows.

--- misctools/activitylog/start-activitylog.bat
+++ misctools/activitylog/start-activitylog.bat
@@ -1,3 +1,5 @@
 rem met 4nt - detach
-detach c:\develop\tcl85\bin\tclsh85.exe c:\nico\nicoprj\misctools\activitylog\activitylog.tcl -l c:\nico\projecten\activitylog\activ -i 5
+rem detach c:\develop\tcl85\bin\tclsh85.exe c:\nico\nicoprj\misctools\activitylog\activitylog.tcl -l c:\nico\projecten\activitylog\activ -i 5
+rem 23-12-2011 nu met sqlite
+detach c:\develop\tcl85\bin\tclsh85.exe c:\nico\nicoprj\misctools\activitylog\activitylog-sqlite.tcl -l c:\nico\projecten\activitylog\activ -i 5
 
