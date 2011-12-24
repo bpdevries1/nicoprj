327e753aad9be18b217b284dca39a87db7d44179
vreeze42 <nico@ndv.nl>
2011-12-24 19:14:19 +0100

    tcl functions to use in sqlite: is_new_time_group and det_group.

---
+++ misctools/activitylog/db_functions.tcl
@@ -0,0 +1,26 @@
+proc is_new_time_group {title} {
+	set result 0
+	# {NONE} is screensaver, report in group
+	# if {$title == "{NONE}"} {set result 1}
+	if {$title == "{NO INFO}"} {set result 1}
+	if {$title == "{STARTED}"} {set result 1}
+	return $result	
+}
+
+proc det_group {title} {
+	global lst_group_regexps
+	if {[regexp {Tools} $title]} {
+		log "Determining group of: $title"
+	}
+	set result "Unknown"
+	foreach el $lst_group_regexps {
+		# nu niet foreach, dan werkt break niet goed
+		lassign $el re group
+		if {[regexp -nocase $re $title]} {
+			set result $group
+			break
+		}
+	}
+	return $result
+}
+
