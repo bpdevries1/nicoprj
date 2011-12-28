5c395b3768d0106baa378a7265b4430543ccec9c
vreeze42 <nico@ndv.nl>
2011-12-28 20:37:39 +0100

    clj-ring tryout, including text file with notes.

---
+++ tryout/clj-ring/project.clj
@@ -0,0 +1,13 @@
+(defproject clj-ring "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [ring "1.0.0-RC1"]
+                 [ring/ring-jetty-adapter "1.0.0-RC1"]
+                 [net.cgrand/moustache "1.1.0"]
+                 [necessary-evil "2.0.0-SNAPSHOT"]
+                 [compojure "0.6.5"]
+                 [hiccup "0.3.7"]])
+
+
+
---
+++ tryout/clj-ring/test/clj_ring/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-ring.test.core
+  (:use [clj-ring.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
