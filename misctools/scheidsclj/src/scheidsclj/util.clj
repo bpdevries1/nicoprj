019be87895d2eae98abe24a41991b77bb7d87919
vreeze42 <nico@ndv.nl>
2012-01-05 20:32:32 +0100

    Removed comments.

--- misctools/scheidsclj/src/scheidsclj/util.clj
+++ misctools/scheidsclj/src/scheidsclj/util.clj
@@ -2,13 +2,6 @@
 (ns scheidsclj.util
   (:use scheidsclj.lib))
 
-; voor new-sol-nr wel 2 functions nodig: 1 die de counter-functie maakt, en 1 die 'em steeds aanroept.
-; this should be possible with one function? with a closure?
-;(def sol-nr-counter (make-counter 0))
-;
-;(defn new-sol-nr []
-;  (sol-nr-counter))
-
 ; 13-3-2011 and now in one function/call
 (def new-sol-nr (make-counter 0))
 
