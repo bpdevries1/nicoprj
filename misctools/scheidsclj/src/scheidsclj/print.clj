31e6a14d2bbccee7b510f32a8e496a0fee236dc6
vreeze42 <nico@ndv.nl>
2012-01-07 22:31:32 +0100

    Added printlnfs, to reduce #printlnf calls in code.

--- misctools/scheidsclj/src/scheidsclj/print.clj
+++ misctools/scheidsclj/src/scheidsclj/print.clj
@@ -40,6 +40,8 @@
     (:lst-can-referee (ar-inp-games game-id))))
 
 ; :as sol-referee not needed in destructuring
+; user=> (apply str (interpose ", " (sort [3 5 6 4 3 9])))
+;"3, 3, 4, 5, 6, 9"
 (defn sol-referee-to-string [{:keys [game-name same-day referee-name whinefactor value game-id]} 
                              ar-inp-games]
   (str game-name " (sd=" same-day ") "
@@ -51,6 +53,44 @@
   "Combination of println and format"
   [fmt & args]
   (println (apply format fmt args)))
+
+; ex: (printlnfs ["%f" 1.32] ["abc"])
+(defn printlnfs
+  "Call printlnf for each argument, argument is a vector with the args to printlnf
+   ex: (printlnfs [\"%f\" 1.32] [\"abc\"])"
+  [& args]
+  (doseq [arg args] (apply printlnf arg)))
+
+; @param sol oplossing
+; @param can-find-better: functie die oplossing als input heeft, en true/false als output.
+(defn print-solution [{:keys [solnr solnr-parent vec-sol-referee 
+                              lst-sol-person-info fitness prod-games-person-day 
+                              sum-whinefactors lst-whinefactors lst-counts 
+                              max-referee n-diff-referee] :as sol}               ; :as sol needed for can-find-better                 
+                      ar-inp-games can-find-better]
+  (printlnfs ["Solution %d (parent: %d)" solnr solnr-parent]
+             ["Games:"])
+  (doseq [sol-referee vec-sol-referee] 
+    (println (sol-referee-to-string sol-referee ar-inp-games)))
+  (println "--------------\nReferees:")
+  ; * 1.0 needed to cast integer to float, otherwise problems with format.
+  (doseq [{:keys [nreferee whinefactor referee-name]} lst-sol-person-info] 
+    (printlnf "#%d zf=%6.1f : %s" nreferee (* 1.0 whinefactor) referee-name)) 
+  (printlnfs ["--------------\nStatistics:"]
+             ["Fitness: %f" fitness]
+             ["Maximum #games for a referee on one day: %d" prod-games-person-day]
+             ["Sum of whine factors: %f" sum-whinefactors]
+             ["List of whine factors: %s" (apply str (interpose ", " (sort lst-whinefactors)))]
+             ["#games per referee: %s" (apply str (interpose ", " (sort lst-counts)))]
+             ["Maximum #games for a referee: %d" max-referee]
+             ["#different referees: %d" n-diff-referee]
+             ["#games: %d" (count vec-sol-referee)])
+
+  (println (if (can-find-better sol)
+     "from this solution a BETTER one can be found with 1 change..."
+     "from this solution a better one CANNOT be found with 1 change..."))
+
+  (println "\n==========\n")) 
 
 ; doall zou moeten werken om lazy te forcen, maar met str werkt dit niet zo
 ; sort werkt wel, ook wel logisch: om te sorten, moet je alle waarden hebben.
@@ -70,7 +110,7 @@
 ; - template file met code hierin, zoals bv in perftoolset/testsuite def. Deze ook in php etc, moet je eigenlijk niet willen.
 ;
 ; eerst pragmatisch: stukken van hieronder wat leesbaarder en meer concise (korter) maken.
-(defn print-solution [{:keys [solnr solnr-parent vec-sol-referee 
+(defn print-solution1 [{:keys [solnr solnr-parent vec-sol-referee 
                               lst-sol-person-info fitness prod-games-person-day 
                               sum-whinefactors lst-whinefactors lst-counts 
                               max-referee n-diff-referee] :as sol}               ; :as sol needed for can-find-better                 
@@ -96,6 +136,7 @@
     (println "from this solution a BETTER one can be found with 1 change...")
     (println "from this solution a better one CANNOT be found with 1 change..."))
   (println "\n==========\n")) 
+
 
 (defn print-best-solution [proposition ar-inp-games can-find-better]
   (let [{:keys [iteration lst-solutions]} @proposition]
