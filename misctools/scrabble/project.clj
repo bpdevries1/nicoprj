fba5344b35e82faefdb1a1faaf458ff8525462da
vreeze42 <nico@ndv.nl>
2012-01-07 22:33:10 +0100

    Scrabble first dabbles: find words in wordlist based on regexp, check if we have enough letters.

---
+++ misctools/scrabble/project.clj
@@ -0,0 +1,6 @@
+(defproject scrabble "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [difform "1.1.2"]                        ; try for have-letters, not used anymore.
+                 [clj-diff "1.0.0-SNAPSHOT"]])            ; try for have-letters, not used anymore.
