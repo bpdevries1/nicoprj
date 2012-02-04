5dc1639f54519a6a553ad0457ec3187c26336deb
vreeze42 <nico@ndv.nl>
2012-02-04 20:59:55 +0100

    Put logs in ~/log.

--- systemtools/backup2nas/backup2nas.sh
+++ systemtools/backup2nas/backup2nas.sh
@@ -6,7 +6,10 @@ cd /home/nico/nicoprj/systemtools/backup2nas
 
 # met onderstaande wel gevonden
 # ./backup-files.tcl -t /media/nas/backups/pcubuntu -paths paths-pcubuntu.txt -r results.txt -p -ignoreregexps ignoreregexps.txt >/tmp/backuptonas.log 2>&1
-./backup-files.tcl -t /media/nas/backups/pcubuntu -paths paths.txt -r results.txt -p -ignoreregexps ignoreregexps.txt >/tmp/backuptonas.log 2>&1
+# ./backup-files.tcl -t /media/nas/backups/pcubuntu -paths paths.txt -r results.txt -p -ignoreregexps ignoreregexps.txt >/tmp/backuptonas.log 2>&1
+
+# 18-1-2012 nu glasvezel, maakt voor deze niet uit, wil log in ~/log hebben.
+./backup-files.tcl -t /media/nas/backups/pcubuntu -paths paths.txt -r results.txt -p -ignoreregexps ignoreregexps.txt >/home/nico/log/backuptonas.log 2>&1
 
 # gebruik onderstaande voor backup van alle bestanden, niet alleen sinds vorige keer.
 # tclsh backup-files.tcl -t /media/nas/backups/pcubuntu -paths paths-pcubuntu.txt -r results.txt -ignoreregexps ignoreregexps.txt >/tmp/backuptonas.log 2>&1
