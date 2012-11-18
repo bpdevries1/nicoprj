078e49265b7b200121847b4779e96a05697c4dd2
vreeze42 <nico@ndv.nl>
2012-11-18 16:29:02 +0100

    sync-log to ~/log now

--- mediatools/organise/sync-music-db.sh
+++ mediatools/organise/sync-music-db.sh
@@ -3,6 +3,6 @@
 # sync albums and singles
 cd /home/nico/nicoprj/mediatools/organise
 
-./sync-music-db.tcl -dir /media/nas/media/Music/Albums >/tmp/sync-music-db-albums.out 2>&1
-./sync-music-db.tcl -dir /media/nas/media/Music/Singles >/tmp/sync-music-db-singles.out 2>&1
+./sync-music-db.tcl -dir /media/nas/media/Music/Albums >/home/nico/log/sync-music-db-albums.out 2>&1
+./sync-music-db.tcl -dir /media/nas/media/Music/Singles >/home/nico/log/sync-music-db-singles.out 2>&1
 
