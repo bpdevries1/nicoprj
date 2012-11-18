9cd0d911e125e7990b32e0d9a3a1e524dfcc93bd
vreeze42 <nico@ndv.nl>
2012-11-18 16:05:28 +0100

    Changed GMZ sentinels

--- tryout/wish-text-tags/gmz.tcl
+++ tryout/wish-text-tags/gmz.tcl
@@ -1,7 +1,7 @@
 proc get_lists {} {
   set lst {}
-  lappend lst [list Sentinel T711 T911 T288 T317 T502]
-  lappend lst [list Script KA GWS GeoL GeoF]
+  lappend lst [list Sentinel T195 T711 T911 T288 T317 T502]
+  lappend lst [list Script KA GWS GeoL GeoF Lotus]
   return $lst
 }
 
