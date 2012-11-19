5d56a9c5ad0a332e18f5cc38311dd22389666b2b
vreeze42 <nico@ndv.nl>
2012-11-19 20:12:36 +0100

    Removed scheids(clj), now in bitbucket

--- misctools/scheidsclj/src/scheidsclj/geneticlib.clj
+++
@@ -1,5 +0,0 @@
-; geneticlib - functions that could be present in a GA lib.
-; or replace these functions by an external available GA lib.
-
-(ns scheidsclj.geneticlib)
-
--- misctools/scheidsclj/test/scheidsclj/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns scheidsclj.test.core
-  (:use [scheidsclj.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
