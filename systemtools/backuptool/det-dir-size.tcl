b90ea4ade189f2907f61a88af23c40ac3d111dd0
vreeze42 <nico@ndv.nl>
2013-03-17 20:15:52 +0100

    Added call to diskfree

--- systemtools/backuptool/det-dir-size.tcl
+++ systemtools/backuptool/det-dir-size.tcl
@@ -27,7 +27,10 @@ proc main {argv} {
   set cmd_args [::cmdline::getoptions argv $options $usage]
   set conn [open_db [dict get $cmd_args db]]
   
-  det_size_dir_root $conn $cmd_args
+  set size_check_id [insert_size_check $conn]
+  # 17-3-2013 NdV nu even niet.
+  # det_size_dir_root $conn $cmd_args
+  det_disk_free $conn $size_check_id
   
   $conn close  
 }
@@ -35,10 +38,12 @@ proc main {argv} {
 proc open_db {db_name} {
   file mkdir [file dirname $db_name]
   # for testing
-  file delete $db_name
+  # file delete $db_name
   
   set conn [tdbc::sqlite3::connection create db $db_name]
   if {[$conn tables] == {}} {
+    log info "Database empty, create tables"
+    breakpoint
     create_db $conn 
   }
   return $conn
@@ -50,15 +55,25 @@ proc create_db {conn} {
   
   # state for long running processes.
   db_eval $conn "create table size_check_todo (size_check_dir_id, path)"
+  
+  # df output
+  db_eval $conn "create table diskfree (id integer primary key autoincrement, size_check_id, filesystem, total_mb, used_mb, free_mb, mounted)"
 }
 
-proc det_size_dir_root {conn cmd_args} {
-  global stmt
-  set path_root [file normalize [dict get $cmd_args path]]
-  set recur_size [dict get $cmd_args recur_size]
+proc insert_size_check {conn} {
   set hostname [det_hostname]
   set ts [clock format [clock seconds] -format "%Y-%m-%d %H:%M:%S"]
   set size_check_id [db_eval $conn "insert into size_check (hostname, ts) values ('$hostname', '$ts')" 1]
+  return $size_check_id
+}
+
+proc det_size_dir_root {conn cmd_args size_check_id} {
+  global stmt
+  set path_root [file normalize [dict get $cmd_args path]]
+  set recur_size [dict get $cmd_args recur_size]
+  #set hostname [det_hostname]
+  #set ts [clock format [clock seconds] -format "%Y-%m-%d %H:%M:%S"]
+  # set size_check_id [db_eval $conn "insert into size_check (hostname, ts) values ('$hostname', '$ts')" 1]
   set stmt(size_check_dir) [$conn prepare "insert into size_check_dir (size_check_id, path, size_mb, last_mod) values ($size_check_id, :path, :size_mb, :last_mod)"]
   set stmt(size_check_todo) [$conn prepare "insert into size_check_todo (size_check_dir_id, path) values (:size_check_dir_id, :path)"]
   set stmt(size_check_todo_delete) [$conn prepare "delete from size_check_todo where size_check_dir_id = :size_check_dir_id"]
@@ -130,6 +145,38 @@ proc exec_du {path ftmp} {
   }
 }
 
+proc exec_df_old {ftmp} { 
+  global tcl_platform
+  if {$tcl_platform(platform) == "windows"} {
+    catch {exec -ignorestderr c:/util/cygwin/bin/df.exe -m >@$ftmp}
+  } elseif {$tcl_platform(platform) == "unix"} {
+    catch {exec -ignorestderr df -m >@$ftmp}
+  } else {
+    error "Unknown platform: $tcl_platform(platform)"  
+  }
+}
+
+proc exec_df {ftmp} {
+  exec_system_cmd $ftmp df {-m}
+}
+
+# @param cmd_args: list of args.
+proc exec_system_cmd {ftmp cmdname {cmd_args {}}} {
+  global tcl_platform
+  if {$tcl_platform(platform) == "windows"} {
+    set cmdexe [file join "c:/util/cygwin/bin" "$cmdname.exe"]
+  } elseif {$tcl_platform(platform) == "unix"} {
+    set cmdexe $cmdname
+  } else {
+    error "Unknown platform: $tcl_platform(platform)"  
+  }
+  try_eval {
+    exec -ignorestderr $cmdexe {*}$cmd_args >@$ftmp
+  } {
+    log warn "sys cmd gave error/warning: $cmdexe: $errorResult" 
+  }
+}
+
 proc det_hostname {} {
   global env
   set hostname ""
@@ -143,7 +190,7 @@ proc det_hostname {} {
   error "Could not determine hostname, both HOSTNAME and COMPUTERNAME in env are empty" 
 }
 
-proc db_eval {conn query {return_id 0}} {
+proc db_eval_old {conn query {return_id 0}} {
   set stmt [$conn prepare $query]
   $stmt execute
   $stmt close
@@ -152,7 +199,7 @@ proc db_eval {conn query {return_id 0}} {
   }
 }
 
-proc stmt_exec {conn stmt dct {return_id 0}} {
+proc stmt_exec_old {conn stmt dct {return_id 0}} {
   $stmt execute $dct
   if {$return_id} {
     return [[$conn getDBhandle] last_insert_rowid]   
@@ -165,5 +212,42 @@ proc read_file {filename} {
   close $f
   return $res
 }
+
+proc det_disk_free {conn size_check_id} {
+  set stmt [$conn prepare "insert into diskfree (size_check_id, filesystem, total_mb, used_mb, free_mb, mounted) values ($size_check_id, :filesystem, :total_mb, :used_mb, :free_mb, :mounted)"]
+  set res [exec_read_file exec_df]
+  foreach line [lrange [split $res "\n"] 1 end] {
+    # @pre filesystem has no spaces, mounted-on can (and does) have spaces
+    lassign $line filesystem total_mb used_mb free_mb
+    set mounted [join [lrange $line 5 end] " "]
+    if {$mounted != ""} {
+      $stmt execute [vars_to_dict size_check_id filesystem total_mb used_mb free_mb mounted]
+    }
+  }
+}
+
+# @param extra_args: list of args to give procname
+proc exec_read_file {procname {extra_args {}}} {
+  set ftmp [file tempfile tempfilename]
+  log debug "Putting output for $procname in $tempfilename"  
+  exec_df {*}$extra_args $ftmp
+  close $ftmp
+  set res [read_file $tempfilename]
+  return $res
+}
+
+###########################
+# library function
+###########################
+proc vars_to_dict {args} {
+  set res {}
+  foreach arg $args {
+    upvar $arg val
+    # puts "$arg = $val"
+    lappend res $arg $val
+  }
+  return $res
+}
+
 
 main $argv
