0ddee198f89e908f2e3d2e51718a3cc9415144a3
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2013-04-12 20:11:33 +0200

    Added tcl-install-packages.sh

---
+++ systemtools/newpc/tcl-install-packages.sh
@@ -0,0 +1,10 @@
+# install tcl packages with teacup
+teacup install cmdline 
+teacup install struct::list
+teacup install Tclx
+teacup install Itcl
+teacup install cmdline 
+teacup install csv
+teacup install Tclx
+teacup install tcom
+  
