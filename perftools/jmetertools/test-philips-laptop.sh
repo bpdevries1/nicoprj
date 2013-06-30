7364155b897300baa88e9b659088420908f2122e
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2013-06-30 21:59:06 +0200

    Made working on Windows by commenting log debug lines.

---
+++ perftools/jmetertools/test-philips-laptop.sh
@@ -0,0 +1,3 @@
+# rm *.log
+./xmljtls2db.tcl c:/nico/Dropbox/Philips/MyPhilips-JanRain c:/aaa/MyPhilips-JanRain
+# ./xmljtls2db.tcl ~/aaa/MyPhilips-JanRain
--- perftools/jmetertools/xmljtls2db.tcl
+++ perftools/jmetertools/xmljtls2db.tcl
@@ -2,13 +2,15 @@
 
 # jtls2db.tcl - convert jtl's (xml) to a sqlite3 db
 
+# @note log debug (and maybe first_line) statements cause memory exhaustion, uncommenting those helps on Windows. (on Linux no problems, but have more memory there).
+
 package require tdbc::sqlite3
 package require xml
 package require Tclx
 package require struct::stack
 package require ndv
 
-set log [::ndv::CLogger::new_logger [file tail [info script]] warn]
+set log [::ndv::CLogger::new_logger [file tail [info script]] info]
 $log set_file "[info script].log"
 
 # @todo ts_utc als extra veld opnemen in httpsample.
@@ -33,6 +35,7 @@ proc main {argv} {
 
 proc create_db {dirname} {
   set db_file [file join $dirname "jtldb.db"]
+  file mkdir $dirname
   file delete -force $db_file
   # sqlite3 db $dbfile
   set conn [open_db $db_file]
@@ -49,11 +52,11 @@ proc create_db {dirname} {
 }
 
 proc finish_db {conn} {
-  log debug "Create index on ts:"
+  # log debug "Create index on ts:"
   db_eval $conn "create index ix_ts on httpsample (ts)"
-  log debug "Create index on lb:"
+  # log debug "Create index on lb:"
   db_eval $conn "create index ix_lb on httpsample (lb)"
-  log debug "Creating indexes finished, closing db"
+  # log debug "Creating indexes finished, closing db"
   $conn close
 }
 
@@ -71,23 +74,33 @@ proc read_jtl {conn jtlfile dct_insert_stmts} {
   # set jtlfile_id [stmt_exec $conn [dict get $dct_insert_stmts jtlfile] [dict create path $jtlfile] 1]
   set jtlfile_id [[dict get $dct_insert_stmts jtlfile] [dict create path $jtlfile] 1]
   log info "jtlfile_id: $jtlfile_id"
-  set parser [::xml::parser -elementstartcommand [list signal_error elementstart count elt_stack] \
-    -elementendcommand [list signal_error elementend elt_stack] \
-    -characterdatacommand [list signal_error characterdata elt_stack] \
-    -defaultcommand [list signal_error xml_default]]
+  if {0} {
+    log info "using dummy callbacks"
+    set parser [::xml::parser -parser expat -elementstartcommand [list signal_error dummy_cb] \
+      -elementendcommand [list signal_error dummy_cb] \
+      -characterdatacommand [list signal_error dummy_cb] \
+      -defaultcommand [list signal_error dummy_cb]]
+  } else {
+    set parser [::xml::parser -parser expat -elementstartcommand [list signal_error elementstart count elt_stack] \
+      -elementendcommand [list signal_error elementend elt_stack] \
+      -characterdatacommand [list signal_error characterdata elt_stack] \
+      -defaultcommand [list signal_error xml_default]]
+  }
   set f [open $jtlfile r]
-  log debug "Reading file: $jtlfile"
+  log info "Reading file: $jtlfile"
   set elt_stack [struct::stack]
   set text [read $f]
-  log debug "Reading file finished (length:[string length $text]), now parsing text"
+  log info "Reading file finished (length:[string length $text]), now parsing text"
   # $parser parse $text
   try_eval {
     $parser parse $text
   } {
-    log debug "error: $errorResult"
-    log debug "Maybe no close tag, because JMeter still running"
+    log warn "error: $errorResult"
+    log warn "Maybe no close tag, because JMeter still running"
   }
-  log debug "Parsing text finished"
+  $parser free
+  # log debug "Parsing text finished"
+  log info "reading jtl finished: $jtlfile"
   db_eval $conn "commit"
 }
 
@@ -102,59 +115,63 @@ proc signal_error {proc_name args} {
   }  
 }
 
+proc dummy_cb {args} {
+  
+}
+
 proc elementstart {count_name elt_stack_name name attlist args} {
   global conn
   upvar #0 $count_name count
   upvar #0 $elt_stack_name elt_stack
-  log debug "elementstart: $name (att: $attlist, args: $args)"
+  # log debug "elementstart: $name (att: $attlist, args: $args)"
   if {[ignore_elt $name]} {
-    log debug "Ignore element: $name"
+    # log debug "Ignore element: $name"
     return 
   }
   incr count
-  log debug "Handled $count elements"
+  # log debug "Handled $count elements"
   if {[expr $count % 1000] == 0} {
-    log debug "Handled $count elements"
+    log info "Handled $count elements, commit and start new transaction."
     db_eval $conn "commit"
     db_eval $conn "begin transaction"
   }
   
   $elt_stack push [dict create tag $name attrs $attlist]
-  log info "pushed elt: size now: [$elt_stack size]"
-  log debug "current element after change: [first_line [$elt_stack peek]]"
-  # log debug "current element after change: [$elt_stack peek]"
-  log debug "elementstart: $name finished"
+  # log debug "pushed elt: size now: [$elt_stack size]"
+  # log debug "current element after change: [first_line [$elt_stack peek]]"
+  # # log debug "current element after change: [$elt_stack peek]"
+  # log debug "elementstart: $name finished"
 }
 
 proc characterdata {elt_stack_name data} {
   upvar #0 $elt_stack_name elt_stack
-  log debug "1.character data: [first_line $data] ***"
+  # log debug "1.character data: [first_line $data] ***"
   # breakpoint
   if {[string trim $data] != ""} {
-    log debug "stack size before change: [$elt_stack size]"
+    # log debug "stack size before change: [$elt_stack size]"
     if {[$elt_stack size] > 0} {
       set elt [$elt_stack pop]
       dict set elt text $data
       $elt_stack push $elt
-      log debug "current element after change: [first_line [$elt_stack peek]]"
+      # log debug "current element after change: [first_line [$elt_stack peek]]"
     } else {
-      log debug "stack is empty, don't change element with character data" 
+      # log debug "stack is empty, don't change element with character data" 
     }
-    log debug "stack size after change: [$elt_stack size]"
+    # log debug "stack size after change: [$elt_stack size]"
   }
-  # log debug "current element after change: [$elt_stack peek]"
-  log debug "character data end."
+  # # log debug "current element after change: [$elt_stack peek]"
+  # log debug "character data end."
 }
 
 proc elementend {elt_stack_name name} {
   upvar #0 $elt_stack_name elt_stack
-  log debug "element end: $name"
+  # log debug "element end: $name"
   if {[ignore_elt $name]} {
-    log debug "Ignore element: $name"
+    # log debug "Ignore element: $name"
     return 
   }
 
-  log info "will pop elt: size now: [$elt_stack size]"
+  # log debug "will pop elt: size now: [$elt_stack size]"
   set child ""
   if {[$elt_stack size] >= 2} {
     set child [$elt_stack pop]
@@ -163,16 +180,16 @@ proc elementend {elt_stack_name name} {
     $elt_stack push $parent
   } elseif {[$elt_stack size] == 1} {
     # toplevel finished? callback?
-    log info "elt_stack size == 1, do callback!"
+    # log debug "elt_stack size == 1, do callback!"
     handle_main_sample [$elt_stack pop]
   } else {
     log error "Stack size < 1 and found end-tag, should not happen" 
   }
-  log info "stack size after change: [$elt_stack size]"
+  # log debug "stack size after change: [$elt_stack size]"
   if {[$elt_stack size] > 0} {
-    log debug "current element after change: [first_line [$elt_stack peek]]"
+    # log debug "current element after change: [first_line [$elt_stack peek]]"
   }
-  log debug "elementend $name: finished"
+  # log debug "elementend $name: finished"
 }
 
 proc ignore_elt {name} {
@@ -182,7 +199,7 @@ proc ignore_elt {name} {
   return 0
 }
 
-proc stack_to_string {elt_stack} {
+proc stack_to_string_old {elt_stack} {
   set res {}
   # stack doesn't correctly handle one stack item which is a list.
   if {[$elt_stack size] == 1} {
@@ -196,7 +213,7 @@ proc stack_to_string {elt_stack} {
   join $res ", "
 }
 
-proc det_latency {elt} {
+proc det_latency_old {elt} {
   set attrs [dict_get $elt attrs {}]
   if {$attrs != {}} {
     dict_get $attrs lt "<none>"
@@ -206,25 +223,25 @@ proc det_latency {elt} {
 }
 
 proc xml_default {data} {
-  log debug "XML default, data=$data" 
+  # log debug "XML default, data=$data" 
 }
 
 proc handle_main_sample {sample} {
   global conn dct_insert_stmts jtlfile_id
-  log info "handle main sample: [first_line $sample]"
-  log info "latency of main sample: [dict get $sample attrs lt]"
+  # log debug "handle main sample: [first_line $sample]"
+  # log debug "latency of main sample: [dict get $sample attrs lt]"
   # breakpoint
   set main_id [insert_sample $sample]
-  log debug "inserted main sample"
+  # log debug "inserted main sample"
   insert_assertion_results $main_id $sample
-  log debug "inserted assertion results"
+  # log debug "inserted assertion results"
   insert_sub_samples $main_id $sample
-  log info "handle main sample: finished"
+  # log debug "handle main sample: finished"
 }
 
 proc insert_sample {sample {parent_id ""}} {
   global conn dct_insert_stmts jtlfile_id
-  log debug "insert_sample: start"
+  # log debug "insert_sample: start"
   set dct [dict_get $sample attrs {}] ; # std attrs like t, ts, ...
   dict set dct jtlfile_id $jtlfile_id
   dict set dct parent_id $parent_id
@@ -242,7 +259,7 @@ proc insert_sample {sample {parent_id ""}} {
   }
   # set main_id [stmt_exec $conn [dict get $dct_insert_stmts httpsample] $dct 1]
   set main_id [[dict get $dct_insert_stmts httpsample] $dct 1]
-  log debug "insert_sample: finished"
+  # log debug "insert_sample: finished"
   return $main_id  
 }
 
@@ -262,16 +279,16 @@ proc insert_assertion_results {sample_id sample} {
 }
 
 proc insert_sub_samples {sample_id sample} {
-  log debug "insert_sub_samples in $sample_id: [first_line $sample]"
+  # log debug "insert_sub_samples in $sample_id: [first_line $sample]"
   global conn dct_insert_stmts jtlfile_id
   foreach sub_elt [dict_get $sample subelts {}] {
     set sub_tag [dict get $sub_elt tag]
     if {$sub_tag == "httpSample"} {
-      log debug "inserting sub_sample: [first_line $sub_elt]"
+      # log debug "inserting sub_sample: [first_line $sub_elt]"
       set sub_id [insert_sample $sub_elt $sample_id]
       insert_assertion_results $sub_id $sub_elt
       insert_sub_samples $sub_id $sub_elt
-      log debug "inserted sub_sample"      
+      # log debug "inserted sub_sample"      
     }
   }
 }
