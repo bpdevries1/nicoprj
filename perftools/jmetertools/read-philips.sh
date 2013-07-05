22dfdb1e0b5312527d3a5d645b4a1cb965beddab
vreeze42 <nico@ndv.nl>
2013-07-05 20:05:13 +0200

    Make test and 'normal' (read) versions of main shell script.

---
+++ perftools/jmetertools/read-philips.sh
@@ -0,0 +1,4 @@
+rm *.log
+./xmljtls2db.tcl ~/Dropbox/Philips/MyPhilips-JanRain ~/aaa/MyPhilips-JanRain
+./handle-myphilips.tcl ~/aaa/MyPhilips-JanRain
+
--- perftools/jmetertools/test-philips.sh
+++ perftools/jmetertools/test-philips.sh
@@ -1,4 +1,4 @@
 rm *.log
-./xmljtls2db.tcl ~/Dropbox/Philips/MyPhilips-JanRain ~/aaa/MyPhilips-JanRain
-./handle-myphilips.tcl ~/aaa/MyPhilips-JanRain
+./xmljtls2db.tcl ~/Dropbox/Philips/MyPhilips-JanRain ~/aaa/MyPhilips-JanRain-test
+./handle-myphilips.tcl ~/aaa/MyPhilips-JanRain-test
 
