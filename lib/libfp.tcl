d01b6533313ca4f4102e748ebdcfbfb07bc2e531
vreeze42 <nico@ndv.nl>
2013-07-14 22:57:43 +0200

    Added more functions and tests, started with map.

--- lib/libfp.tcl
+++ lib/libfp.tcl
@@ -14,6 +14,7 @@
 # @note ? and - are possible in proc names. ':' also, already used for dict accessors, see libdict.
 # @note what to do with lazy handling, eg in if-proc.
 # @note use Tcl syntax/formatting and Clojure-like formatting both, most applicable.
+# @note order is as in clojure: if a function A uses another function B, B should be defined before A.
 
 ##
 # @note some easy, helper functions first
@@ -25,22 +26,93 @@ proc = {a b} {
   }
 }
 
+# this is the if from clojure, don't want to override the std Tcl def.
+# @todo handle expressions as first argument? Or should have been evaluated before?
+# how to handle nil or empty list?
+# 0 is truthy in clojure, but would not be handy in Tcl.
+# nil is falsy, but '() and [] are seen as truthy, seq function used to convert '() to nil.
+# @note 'yes' and 'no' will be evaluated lazily, only when condition is met.
+# @note therefore the yes and no values should be enclosed in {}.
+# @note OTOH this makes function less handy to use, and would violate a starting point.
+# @note so should either use the standard if (which is 'lazy') or create own other variant.
+proc ifp {test yes no} {
+  if {$test == "nil"} {
+    return $no
+    # eval $no
+    # uplevel 1 $no - default is level 1
+    # uplevel $no
+  } elseif {$test} {
+    return $yes
+    # eval $yes
+  } else {
+    return $no
+    # eval $no
+  }
+}
+
+# @note seq for now just to translate empty list to nil, and this becomes falsy in [not] and [ifp]
+proc seq {l} {
+  ifp [= [string length $l] 0] nil $l
+}
+
+proc empty? {l} {
+  ifp [= [seq $l] nil] 1 0  
+}
+
+proc cond_1 {args} {
+  lassign $args test result rest
+  puts "cond called with $args"
+  # ifp [empty? $args] 0 [ifp $test $result [cond {*}$rest]]
+  ifp [empty? $args] 0 {[ifp $test $result [cond {*}$rest]]}
+}
+
+# @note ifp not usable, as it is not lazy.
+proc cond {args} {
+  set rest [lassign $args test result]
+  # puts "cond called with $args"
+  # ifp [empty? $args] 0 [ifp $test $result [cond {*}$rest]]
+  # ifp [empty? $args] 0 {[ifp $test $result [cond {*}$rest]]}
+  if {[expr [llength $args] % 2] == 1} {
+    error "cond should be called with an even number of arguments, got $args" 
+  }
+  if {[empty? $args]} {
+    return 0 
+  } elseif {$test} {
+    return $result 
+  } else {
+    cond {*}$rest 
+  }
+}
+
+
 proc not {a} {
- if {$a} {
-   return 0 
- } else {
-   return 1 
- }
+  ifp $a 0 1
 }
 
 proc not= {a b} {
-  not {= $a $b}  
+  # @todo not {= $a $b} should also work?
+  not [= $a $b]  
 }
+
+proc str {args} {
+  join $args ""
+}
+
+# clj fn is also called identity, not iden or id
+proc identity {a} {
+  return $a 
+}
+
 
 # @todo maybe a 'proc fn' to create function-objects, lambda's?
 
+# @todo handle more than one map-var, for traversing more than one map at the same time?
+# @note should handle 2 forms:
+# (map var list expression-with-var)
+# (map lambda list), where lambda is {var expr-with-var}
 proc map {args} {
-
+  lmap {*}$args
+  # note idea is to use cond to test for 2 or 3 arguments: 2 is with lambda, 3 is with var, list, expr
 }
 
 proc filter {args} {
--- lib/test/test-libfp.tcl
+++ lib/test/test-libfp.tcl
@@ -33,10 +33,48 @@ test eq-8 {equals 8} -body {= 1} -returnCodes error -result {wrong # args: shoul
 test eq-9 {equals 9} -body {= 1 1 1} -returnCodes error -result {wrong # args: should be "= a b"}
 test eq-10 {equals 10} -body {= 1 1 13} -returnCodes error -result {wrong # args: should be "= a b"}
 
+test not-1 {not 1} {not 1} 0
+test not-2 {not 2} {not 0} 1
+test not-3 {not 3} {not nil} 1
+
+test not-eq-1 {not equals 1} {not= 0 0} 0
+test not-eq-1 {not equals 1} {not= 0 1} 1
+
+test str-1 {str 1} {str a b} ab
+test str-2 {str 2} {str} ""
+test str-3 {str 3} {str "abc"} "abc"
+test str-4 {str 4} {str 12 "abc" 3} "12abc3"
+
+test iden-1 {iden 1} {identity 42} 42
+test iden-2 {iden 2} {identity {}} {}
+test iden-3 {iden 3} {identity ""} ""
+
+test ifp-1 {ifp 1} {ifp 0 1 2} 2
+test ifp-1 {ifp 1} {ifp 1 1 2} 1
+test ifp-1 {ifp 1} {ifp nil 1 2} 2
+
+test seq-1 {seq 1} {seq {}} nil
+test seq-2 {seq 2} {seq {a b c}} {a b c}
+
+test empty-1 {empty 1} {empty? nil} 1
+test empty-2 {empty 2} {empty? {}} 1
+test empty-3 {empty 3} {empty? {a b}} 0
+
+test cond-1 {cond 1} {cond} 0
+test cond-3 {cond 3} {cond 1 2} 2
+test cond-4 {cond 4} {cond 0 2} 0
+test cond-5 {cond 5} {cond 1 2 3 4} 2
+test cond-6 {cond 6} {cond 0 2 3 4} 4
+test cond-2 {cond 2} -body {cond 1} -returnCodes error -result {cond should be called with an even number of arguments, got 1}
+
 ## test map ##
+# set fields [map x [:fields $table] {ifp [eq $x "id"] "id integer primary key autoincrement" $x}]
+test map-1 {map 1} {map x {id field1 field2} \
+  {ifp [= $x "id"] "id integer primary key autoincrement" $x}} \
+  {{id integer primary key autoincrement} field1 field2}
 
-
-
+# @note more tests with lambda, use with apply?  
+  
 ## test filter ##
 
 
