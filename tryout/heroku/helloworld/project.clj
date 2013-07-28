aecb4981329a885e98f5a4f0b86555ba4df9dbc8
vreeze42 <nico@ndv.nl>
2013-07-28 12:24:18 +0200

    Heroku Clojure Hello World init.

---
+++ tryout/heroku/helloworld/project.clj
@@ -0,0 +1,17 @@
+(defproject helloworld "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://helloworld.herokuapp.com"
+  :license {:name "FIXME: choose"
+            :url "http://example.com/FIXME"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.1"]
+                 [ring/ring-jetty-adapter "1.1.6"]
+                 [ring/ring-devel "1.1.0"]
+                 [ring-basic-authentication "1.0.1"]
+                 [environ "0.2.1"]
+                 [com.cemerick/drawbridge "0.0.6"]]
+  :min-lein-version "2.0.0"
+  :uberjar-name "helloworld-standalone.jar"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :profiles {:production {:env {:production true}}})
