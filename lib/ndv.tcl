ea082b5c501dd14c63ae8aa5e9ff169aa9184c02
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2013-08-03 10:27:37 +0200

    Added lincsv.tcl, read csv file into a list of dictionaries.

--- lib/ndv.tcl
+++ lib/ndv.tcl
@@ -33,3 +33,5 @@ source [file join [file dirname [info script]] libdict.tcl]
 # 27-7-2013 added libdb.tcl (as replacement to be for libsqlite.tcl and db/* (mysql) libraries.
 source [file join [file dirname [info script]] libdb.tcl]
 
+# 2-8-2013 added libcsv.tcl
+source [file join [file dirname [info script]] libcsv.tcl]
