f422871a53a5bc292d02609c5487baa40616fa41
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2013-09-06 20:14:07 +0200

    dashboard tools updated, lots of changes.

---
+++ perftools/dashboardtools/handle-dl.sh
@@ -0,0 +1,3 @@
+./copy-dbs-analysis.tcl -targetdir c:/projecten/Philips/KN-AN-DL -config dealerlocator.txt
+./apidata2dashboarddb.tcl -dir c:/projecten/Philips/Dashboards-DL -srcdir c:/projecten/Philips/KN-AN-DL -srcpattern "*"
+
