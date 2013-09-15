1d7e1755c81280562c733d3b1588360e0e1e6e8f
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2013-09-15 18:07:53 +0200

    Just read MyPhilips keynote data.

---
+++ perftools/keynotetools/scatter2db-myphilips.sh
@@ -0,0 +1,7 @@
+./scatter2db.tcl -nopost -dir /cygdrive/c/projecten/Philips/KNDL/MyPhilips-CN -nomain
+./scatter2db.tcl -nopost -dir /cygdrive/c/projecten/Philips/KNDL/MyPhilips-DE -nomain
+./scatter2db.tcl -nopost -dir /cygdrive/c/projecten/Philips/KNDL/MyPhilips-FR -nomain
+./scatter2db.tcl -nopost -dir /cygdrive/c/projecten/Philips/KNDL/MyPhilips-RU -nomain
+./scatter2db.tcl -nopost -dir /cygdrive/c/projecten/Philips/KNDL/MyPhilips-UK -nomain
+./scatter2db.tcl -nopost -dir /cygdrive/c/projecten/Philips/KNDL/MyPhilips-US -nomain
+
