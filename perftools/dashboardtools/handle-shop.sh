7e1cb6b6861675724b2241b294a97c164aee600b
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2013-10-12 11:08:25 +0200

    Added handle-shop.sh

---
+++ perftools/dashboardtools/handle-shop.sh
@@ -0,0 +1,6 @@
+# ./copy-dbs-analysis.tcl -targetdir c:/projecten/Philips/KN-AN-Shop -srcpattern "Shop-Browsing-Flow-*"
+#../keynotetools/postproclogs.tcl -srcdir /cygdrive/c/projecten/Philips/KN-AN-Shop
+#./apidata2dashboarddb.tcl -dir /cygdrive/c/projecten/Philips/Dashboards-Shop -srcdir /cygdrive/c/projecten/Philips/KN-AN-Shop
+c:/develop/R/R-2.15.3/bin/Rscript.exe c:/nico/nicoprj/R/keynote/graphs-shop.R
+
+
