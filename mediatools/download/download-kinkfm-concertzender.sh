9582c1189a731f971610f756f828772eecfe8330
vreeze42 <nico@ndv.nl>
2013-10-13 15:35:33 +0200

    Added download script for KinkFM X-Rated

---
+++ mediatools/download/download-kinkfm-concertzender.sh
@@ -0,0 +1,3 @@
+# cd /home/nico/nicoprj/mediatools/download
+/home/nico/bin/tclsh86 /home/nico/nicoprj/mediatools/download/download-kinkfm-concertzender.tcl >/home/nico/log/download-kinkfm-concertzender.log 2>&1
+
