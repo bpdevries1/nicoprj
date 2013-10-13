fe172be2961d95281fd9f2ac5641b3572fa5ca7c
vreeze42 <nico@ndv.nl>
2013-10-13 15:34:14 +0200

    Also handle 'read' dirs.

--- perftools/keynotetools/cleanup-backup.tcl
+++ perftools/keynotetools/cleanup-backup.tcl
@@ -3,7 +3,9 @@
 proc main {} {
   set root_dir "/media/nas/backups/PhilipsLaptop/c/projecten/Philips/KNDL"
   foreach dir [glob -type d -directory $root_dir *] {
-    handle_dir $dir 
+    handle_dir $dir
+    # 12-10-2013 also delete files in 'read' folder, they are archived in the 'archief' folder now.
+    handle_dir [file join $dir read]
   }
 }
 
