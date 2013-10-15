027e0c35a60a89c200ee06debd72de31a2975347
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2013-10-15 15:57:41 +0200

    Added tradedoubler shell scripts.

---
+++ perftools/philips/tradedoubler-all.sql
@@ -0,0 +1,27 @@
+-- two queries needed.
+
+drop table if exists page_td1;
+
+-- for all dates, not just from 30-09-2013 onwards.
+create table page_td1 as
+select p.scriptrun_id scriptrun_id, p.id page_id, 1*p.page_seq page_seq, p.ts_cet ts_cet,
+  round(0.001*p.delta_user_msec,3) page_sec, round(min(0.001*i.start_msec),3) start_sec_td,
+  count(*) nelt_td, round(sum(0.001*i.element_delta),3) sec_elt_td,
+  round(0.001 * p.delta_user_msec - min(0.001*i.start_msec),3) sec_overhead_max
+from page p join pageitem i on i.page_id = p.id
+where i.topdomain = 'tradedoubler.com'
+group by 1,2,3,4,5;
+
+create index ix_page_td1 on page_td1 (page_id);
+
+drop table if exists page_td2;
+
+create table page_td2 as
+select t1.*, 
+  round(sum(0.001*i.element_delta),3) sec_elt_after_td,
+  round(max(0, sec_overhead_max - sum(0.001*i.element_delta)),3) sec_no_network_after_td_min 
+from page_td1 t1
+  join pageitem i on i.page_id = t1.page_id
+where 0.001*i.start_msec >= t1.start_sec_td
+group by t1.page_id;
+
