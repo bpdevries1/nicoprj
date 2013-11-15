c673d0f903cd8ffdc8524de4a69b554d4a39fbf2
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2013-11-15 13:42:12 +0100

    Added scatter2db-test-laptop.sh

---
+++ perftools/keynotetools/scatter2db-test-laptop.sh
@@ -0,0 +1,3 @@
+# ./scatter2db.tcl -dir ~/Ymor/Philips/KNDL-test -nopost -debug -loglevel debug -moveread
+./scatter2db.tcl -dir c:/projecten/Philips/KNDL-test -nopost -debug -loglevel debug -moveread -actions dailystats,gt3,maxitem,aggrsub,analyze
+
