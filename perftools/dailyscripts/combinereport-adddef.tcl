bda362b21fcc093dc8b43d8dc5b7d3d27f4bbdcd
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2014-01-24 18:57:11 +0100

    Changes for perftools/dailyscripts - adddef: fill in code to exec

--- perftools/dailyscripts/combinereport-adddef.tcl
+++ perftools/dailyscripts/combinereport-adddef.tcl
@@ -28,19 +28,47 @@ proc main {argv} {
   set dargv [getoptions argv $options $usage]
   
   set db [get_combine_report_db $dargv]
-  set dct [dict create cmds [file normalize [from_cygwin [:cmdfile $dargv]]] srcdir [from_cygwin [:srcdir $dargv]] \
+  set cmds [det_cmds $dargv]
+  set targetdir [from_cygwin [:targetdir $dargv]]
+  # make possible old definition inactive.
+  $db exec2 "update combinedef set active = 0 where targetdir = '$targetdir'" -log
+  set dct [dict create cmds $cmds srcdir [from_cygwin [:srcdir $dargv]] \
     active 1 srcpattern [:pattern $dargv] \
-    targetdir [from_cygwin [:targetdir $dargv]]]
+    targetdir $targetdir]
   set cd_id [$db insert combinedef $dct 1]
   set ndirs 0
   foreach subdir [glob -directory [from_cygwin [:srcdir $dargv]] -type d [:pattern $dargv]] {
     $db insert combinedefdir [dict create combinedef_id $cd_id dir $subdir]
+    delete_dailystatus_combinereport $subdir
     incr ndirs
   }
   $db exec2 "update combinedef set ndirs = $ndirs where id = $cd_id"
-  # set page_id [$db insert page $dctp 1]
+  $db close
+}
+
+proc det_cmds {dargv} {
+  set cmds [file normalize [from_cygwin [:cmdfile $dargv]]]
+  if {$cmds == ""} {
+    set outrootdir [file join [:targetdir $dargv] daily graphs]
+    set combineddb [file join [:targetdir $dargv] daily daily.db]
+    # note newlines added explicitly for now, seems necessary.
+    set cmds "set script_dir \[file join \[info script\] .. .. graphtools\]\r\n
+ndv::source_once \[file join \$script_dir R-wrapper.tcl\]\r\n
+foreach libname \[glob -nocomplain -directory \$script_dir lib*.tcl\] {\r\n
+  ndv::source_once \$libname\r\n
+}\r\n
+set dct \[dict create rootdir \"c:/projecten/Philips/KNDL\" outrootdir \"$outrootdir\" pattern \"XX\" outformat \"png\" loglevel \"info\" combineddb \"$combineddb\" actions \"\" periods \"all\" keepcmd 0 incr 0\]\r\n
+make_graphs \[dict merge \$dct \[dict create combinedactions \"all\"\]\]\r\n"
+  }
+  # check newlines.
+  return $cmds
+}
+
+# delete dailystatus record from src-db where actiontype = 'combinereport', so data will be copied to new aggr location.
+proc delete_dailystatus_combinereport {subdir} {
+  set db [dbwrapper new [file join $subdir keynotelogs.db]]
+  $db exec2 "delete from dailystatus where actiontype = 'combinereport'" -try -log
   $db close
 }
 
 main $argv
-
