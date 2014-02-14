e52006c5b5c822b5a45a6b0f729af916e7d88c87
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2014-02-14 14:06:07 +0100

    add select and output gathering.

--- dbtools/dbscript.tcl
+++ dbtools/dbscript.tcl
@@ -15,6 +15,7 @@ proc main {argv} {
     {rootdir.arg "" "Root directory that contains db's."}
     {dbpattern.arg "*.db" "Databases within rootdir to exec script in"}
     {script.arg "" "script.sql to execute"}
+    {output.arg "" "If set, file to send query output to"}
     {coe "If set, continue-on-error"}
     {dryrun "If set, just print databases and statements, don't exec anything"}
     {loglevel.arg "info" "Log level (debug, info, warn)"}
@@ -27,8 +28,15 @@ proc main {argv} {
 }
 
 proc do_script_dbs {dargv} {
+  global fo
   # first handle single db argument, then rootdir/dbpattern combination.
   set stmts [det_statements [:script $dargv]]
+  if {[:output $dargv] != ""} {
+    set fo [open [:output $dargv] w]
+    puts "Executing script: [:script $dargv] for DB's [:pattern $dargv]"
+  } else {
+    set fo ""
+  }
   # set stmts [-> $dargv :script det_statements] ; possible, just like clojure?
   if {[:db $dargv] != ""} {
     do_statements_db [:db $dargv] $stmts $dargv 
@@ -37,6 +45,9 @@ proc do_script_dbs {dargv} {
     foreach dbname [fileutil::findByPattern [:rootdir $dargv] [:dbpattern $dargv]] {
       do_statements_db $dbname $stmts $dargv
     }
+  }
+  if {$fo != ""} {
+    close $fo
   }
 }
 
@@ -51,6 +62,7 @@ proc det_statements {scriptname} {
 }
 
 proc do_statements_db {dbname stmts dargv} {
+  global fo
   log info "Opened connection to: $dbname"
   set db [dbwrapper new $dbname]
   if {[:coe $dargv]} {
@@ -63,11 +75,43 @@ proc do_statements_db {dbname stmts dargv} {
       log info "Dry run: $stmt"
     } else {
       log info "Executing statement: $stmt"
-      $db exec2 $stmt -log $try
+      if {[query_type $stmt] == "select"} {
+        if {$fo != ""} {
+          puts $fo "DB: $dbname - results:"
+          set res [$db query $stmt]
+          puts $fo [res2table $res]     
+          flush $fo
+        } else {
+          log warn "select statement without output file set, not executing: $stmt"
+        }
+      } else {
+        $db exec2 $stmt -log $try
+      }
     }
   }  
   $db close  
   log info "Closed connection to: $dbname"
+}
+
+proc query_type {stmt} {
+  if {[regexp -nocase {^select } $stmt]} {
+    return "select"
+  } else {
+    return "change"
+  }
+}
+
+# convert resultset (list of dicts) to a tab-seperated table.
+# @todo? put lines (ascii-art) in result
+proc res2table {res} {
+  if {[llength $res] == 0} {
+    return "No results"
+  } else {
+    # example: listc {$i * $i} i <- {1 2 3 4 5} {$i % 2 == 0} => 4 16
+    set header [join [dict keys [lindex $res 0]] "\t"]
+    set rows [listc {[join [dict values $row] "\t"]} row <- $res]
+    return "$header\n[join $rows "\n"]"
+  }
 }
 
 main $argv
