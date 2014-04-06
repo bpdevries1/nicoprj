7cdc38f80bdd76e58d94c8f69d1fd8d94d3d9950
vreeze42 <nico@ndv.nl>
2014-04-06 11:32:07 +0200

    Remove duplicate use of clojure.java.io

--- clojure/lib/def-libs.clj
+++ clojure/lib/def-libs.clj
@@ -3,8 +3,6 @@
 (use '[leiningen.exec :only  (deps)])
 (deps '[[me.raynes/fs "1.4.5"]]) ; file system utilities
 
-(require '[clojure.java.io :as io])
-
 ;(import 'java.util.Date)
 ;(import 'java.io.File)
 ; (import 'java.nio.file.Files) ; -> faalt: dingen in classpath erbij, nieuwe JVM? idd, java 7 nodig, maar nu eerst fs 1.4.5 doet het ook.
