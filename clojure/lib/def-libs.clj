3c063159e8beb592026824b1a9bf07f69800d2a5
vreeze42 <nico@ndv.nl>
2014-05-09 14:11:26 +0200

    Changes for clojure/lib at 2014-05-09 14:09:26

--- clojure/lib/def-libs.clj
+++ clojure/lib/def-libs.clj
@@ -2,12 +2,20 @@
 
 (use '[leiningen.exec :only  (deps)])
 (deps '[[me.raynes/fs "1.4.5"]]) ; file system utilities
+(deps '[[org.clojure/tools.cli "0.2.4"]])
 
-;(import 'java.util.Date)
-;(import 'java.io.File)
-; (import 'java.nio.file.Files) ; -> faalt: dingen in classpath erbij, nieuwe JVM? idd, java 7 nodig, maar nu eerst fs 1.4.5 doet het ook.
+; use newest jdbc by defailt, has incompatbile changes from 0.2.3 to 0.3.0.
+(deps '[[org.clojure/java.jdbc "0.3.3"]
+        [org.xerial/sqlite-jdbc "3.7.2"]]) ; 3.7.2 lijkt nog wel de nieuwste ([2014-05-03 22:39:14])
+
+; orig, working:
+;(deps '[[org.clojure/java.jdbc "0.1.1"]
+;        [org.xerial/sqlite-jdbc "3.7.2"]])
 
 (require '[clojure.java.io :as io] 
          '[clojure.string :as str]
-         '[me.raynes.fs :as fs]) 
+         '[me.raynes.fs :as fs]
+         '[clojure.java.jdbc :as jdbc]
+         '[clojure.tools.cli :refer [cli]])
+
 
