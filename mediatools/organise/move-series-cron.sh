6c0752566f8904406d372d18f627c9a4e5478431
vreeze42 <nico@ndv.nl>
2014-08-10 11:40:03 +0200

    Added move-series-cron.sh

---
+++ mediatools/organise/move-series-cron.sh
@@ -0,0 +1,4 @@
+cd /home/nico/nicoprj/mediatools/organise
+# /home/nico/bin/lein exec /home/nico/nicoprj/systemtools/diskcatalog/do-cron.clj >/home/nico/log/diskcatalog-do-cron.log 2>&1
+/home/nico/bin/lein exec /home/nico/nicoprj/mediatools/organise/move-series.clj >/home/nico/log/move-series-cron.log 2>&1
+
