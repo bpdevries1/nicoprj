a06b9036deb5f21b8673ac59df6c1520a16b4d56
vreeze42 <nico@ndv.nl>
2014-08-10 14:46:12 +0200

    Bugfixes on liberator example

--- tryout/clj-webdev/liberator-service/project.clj
+++ tryout/clj-webdev/liberator-service/project.clj
@@ -10,7 +10,8 @@
                  [hiccup "1.0.5"]
                  [ring-server "0.3.1"]
                  [liberator "0.11.0"]
-                 [cheshire "5.3.1"]]
+                 [cheshire "5.3.1"]
+                 [lib-noir "0.7.6"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler liberator-service.handler/app
          :init liberator-service.handler/init
--- tryout/clj-webdev/liberator-service/src/liberator_service/routes/home.clj
+++ tryout/clj-webdev/liberator-service/src/liberator_service/routes/home.clj
@@ -2,17 +2,51 @@
   (:require [compojure.core :refer :all]
             ;[liberator-service.views.layout :as layout]))
             [liberator.core
-             :refer [defresource resource request-method-in]]))
-
-;(defn home []
-;  (layout/common [:h1 "Hello World!"]))
+             :refer [defresource resource request-method-in]]
+            [cheshire.core :refer [generate-string]]
+            [noir.io :as io]
+            [clojure.java.io :refer [file]]))
 
 (defresource home
-  ;allowed-methods [:get]
-  :handle-ok "Hello World13!"
-  :etag "fixed-etag2"
-  :available-media-types ["text/plain"])
+  :available-media-types ["text/html"]
 
+  :exists?
+  (fn [context]
+    [(io/get-resource "/home.html")
+     {::file (file (str (io/resource-path) "/home.html"))}])
+
+  :handle-ok
+  (fn [{{{resource :resource} :route-params} :request}]
+    (clojure.java.io/input-stream (io/get-resource "/home.html")))
+
+  :last-modified
+  (fn [{{{resource :resource} :route-params} :request}]
+    (.lastModified (file (str (io/resource-path) "/home.html")))))
+
+
+(def users (atom ["John" "Jane"]))
+
+(defresource get-users
+  :allowed-methods [:get]
+  :handle-ok (fn [_] (generate-string @users))
+  :available-media-types ["application/json"])
+
+(defresource add-user
+  :allowed-methods [:post]
+  :malformed? (fn [context]
+                (let [params (get-in context [:request :form-params])]
+                  (empty? (get params "user"))))
+  :handle-malformed "user name cannot be empty!"
+  :post!
+  (fn [context]
+    (let [params (get-in context [:request :form-params])]
+      (swap! users conj (get params "user"))))
+  :handle-created (fn [_] (generate-string @users))
+  :available-media-types ["application/json"])
+
+; routes wel nodig, alleen defresource is niet genoeg!
 (defroutes home-routes
-  (ANY "/" request home))
+  (ANY "/" request home)
+  (ANY "/add-user" request add-user)
+  (ANY "/users" request get-users))
 
