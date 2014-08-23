f28acd7ca0d39874da1b0bfac63d423e5a96262f
vreeze42 <nico@ndv.nl>
2014-08-23 13:06:41 +0200

    Added lsmovies.sh

---
+++ mediatools/organise/lsmovies.sh
@@ -0,0 +1,8 @@
+rm ~/projecten/movies/movies.txt
+
+echo Generated on: `date` >~/projecten/movies/movies.txt
+ls -lR ~/media/tijdelijk/films >>~/projecten/movies/movies.txt
+ls -lR /media/nico/Iomega\ HDD/media/Films >>~/projecten/movies/movies.txt
+
+echo list of movies available in ~/projecten/movies/movies.txt
+
