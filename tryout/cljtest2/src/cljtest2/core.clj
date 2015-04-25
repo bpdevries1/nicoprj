dc43c8e0f8473e5e53d42e9ccb75aa83a72cf461
vreeze42 <nico@ndv.nl>
2015-04-25 12:54:49 +0200

    Changes for ../../tryout at 2015-04-25 12:48:52

---
+++ tryout/cljtest2/src/cljtest2/core.clj
@@ -0,0 +1,1 @@
+(ns cljtest2.core)
---
+++ tryout/cljtest2/test/cljtest2/test/core.clj
@@ -0,0 +1,6 @@
+(ns cljtest2.test.core
+  (:use [cljtest2.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
