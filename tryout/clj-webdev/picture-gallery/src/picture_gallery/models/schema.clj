0ddb7e38e846299ec29fb495f8be362595fa5abb
vreeze42 <nico@ndv.nl>
2015-04-25 12:54:49 +0200

    Changes for ../../tryout/clj-webdev/picture-gallery/src/picture_gallery/models at 2015-04-25 12:48:52

--- tryout/clj-webdev/picture-gallery/src/picture_gallery/models/schema.clj
+++ tryout/clj-webdev/picture-gallery/src/picture_gallery/models/schema.clj
@@ -1,17 +1,22 @@
 (ns picture-gallery.models.schema
   (:require [picture-gallery.models.db :refer :all]
-            [clojure.java.jdbc :as sql]))
+            ;  [clojure.java.jdbc :as sql]
+            [korma.db :refer [defdb transaction]]
+            [korma.core :refer :all]))
 
-(defn create-users-table []
-  (sql/with-connection db
-     (sql/create-table
-      :users
-      [:id "varchar(32) PRIMARY KEY"]
-      [:pass "varchar(100)"])))
+; 16-11-2014 kan Lobos hiervoor gebruiken, nu even niet.
 
-(defn create-images-table []
-  (sql/with-connection db
-    (sql/create-table
-     :images
-     [:userid "varchar(32)"]
-     [:name "varchar(100)"])))
+
+;; (defn create-users-table []
+;;   (sql/with-connection db
+;;      (sql/create-table
+;;       :users
+;;       [:id "varchar(32) PRIMARY KEY"]
+;;       [:pass "varchar(100)"])))
+
+;; (defn create-images-table []
+;;   (sql/with-connection db
+;;     (sql/create-table
+;;      :images
+;;      [:userid "varchar(32)"]
+;;      [:name "varchar(100)"])))
