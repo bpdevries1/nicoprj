60d395c8fc98b865871e21783c4c63cc6573d220
vreeze42 <nico@ndv.nl>
2015-06-15 20:51:08 +0200

    Changed dir locations

--- mediatools/organise/lsmovies.sh
+++ mediatools/organise/lsmovies.sh
@@ -1,8 +1,8 @@
 rm ~/projecten/movies/movies.txt
 
 echo Generated on: `date` >~/projecten/movies/movies.txt
-ls -lR ~/media/tijdelijk/films >>~/projecten/movies/movies.txt
-ls -lR /media/nico/data3tb/media/Films >>~/projecten/movies/movies.txt
+ls -lR /home/media/tijdelijk/films >>~/projecten/movies/movies.txt
+ls -lR /home/media/Films >>~/projecten/movies/movies.txt
 
 echo list of movies available in ~/projecten/movies/movies.txt
 
