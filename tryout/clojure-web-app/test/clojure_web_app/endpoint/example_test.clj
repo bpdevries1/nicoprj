ecb6e03832e7001e7e50894189db3dfc72cbb0e7
vreeze42 <nico@ndv.nl>
2015-10-18 10:30:34 +0200

    Tryout scripts for Clojure Web apps

---
+++ tryout/clojure-web-app/test/clojure_web_app/endpoint/example_test.clj
@@ -0,0 +1,15 @@
+(ns clojure-web-app.endpoint.example-test
+  (:require [com.stuartsierra.component :as component]
+            [clojure.test :refer :all]
+            [kerodon.core :refer :all]
+            [kerodon.test :refer :all]
+            [clojure-web-app.endpoint.example :as example]))
+
+(def handler
+  (example/example-endpoint {}))
+
+(deftest smoke-test
+  (testing "index page exists"
+    (-> (session handler)
+        (visit "/")
+        (has (status? 200) "page exists"))))
