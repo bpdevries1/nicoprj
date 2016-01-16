826d9628ace3b7e3a50e25e7dfe87b51e65141b5
vreeze42 <nico@ndv.nl>
2016-01-16 14:24:39 +0100

    Make usable for pomo(doro)

--- misctools/alarm/alarm.tcl
+++ misctools/alarm/alarm.tcl
@@ -1,4 +1,6 @@
-#!/home/nico/bin/tclsh
+#!/usr/bin/env wish861
+
+#!/home/nico/bin/wish
 
 # test database connection with perftoolset like CDatabase etc.
 
@@ -13,13 +15,12 @@ proc main {argc argv} {
 
   $log debug "argv: $argv"
   set options {
-    {after.arg "90" "Sound first alarm after <after> seconds"}
-    {repeat.arg "60" "Repeat alarm after <repeat> seconds"}
+    {after.arg "1500" "Sound first alarm after <after> seconds (def 25m)"}
+    {repeat.arg "0" "Repeat alarm after <repeat> seconds"}
     {loglevel.arg "" "Zet globaal log level"}
   }
   set usage ": [file tail [info script]] \[options] :"
   array set ar_argv [::cmdline::getoptions argv $options $usage]
-  
 
   wm withdraw .
   after [expr $ar_argv(after)*1000] alarm
@@ -34,10 +35,11 @@ proc alarm {} {
   $log info "Alarm!"
   
   # eerst amarok stoppen, anders sound-card gelocked
-  exec dcop amarok player stop
+  # 16-1-2016 dcop doet het niet meer, nu meestal ook amarok stil, zeker tijdens pomodore.
+  # exec dcop amarok player stop
   # wel tijdje wachten, 1 sec is niet genoeg.
-  after 10000 
-  $log debug "Amarok gestop en 10 sec gewacht, nu alarm!"
+  # after 10000 
+  # $log debug "Amarok gestopt en 10 sec gewacht, nu alarm!"
   # werkt dus ook niet, probeert tcl bij starten al soundcard te claimen?
   # dan een ander process starten of met crontab werken?
   
@@ -45,8 +47,17 @@ proc alarm {} {
   snack::sound s
   $f configure 440
   s stop
-  s play -filter $f  
-  after [expr $ar_argv(repeat)*1000] alarm
+  s play -filter $f
+
+  set answer [tk_messageBox -message "Alarm!" \
+                  -icon info -type ok \
+                  -detail "$ar_argv(after) seconds have elapsed."]
+  
+  if {$ar_argv(repeat) > 0} {
+    after [expr $ar_argv(repeat)*1000] alarm  
+  } else {
+    exit
+  }
 }
 
 main $argc $argv
