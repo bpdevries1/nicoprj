4e6791aa36573c5e57ec07c705318e5b486a6377
vreeze42 <nico@ndv.nl>
2016-01-19 22:19:08 +0100

    Add more tables

--- mediatools/mediaweb/src/mediaweb/models/entities.clj
+++ mediatools/mediaweb/src/mediaweb/models/entities.clj
@@ -8,53 +8,73 @@
             [libndv.coerce :refer [to-float to-int to-key]]))
 
 (declare action)
+(declare author)
 (declare book)
+(declare bookauthor)
 (declare bookformat)
 (declare directory)
 (declare file)
-(declare refile)
+(declare relfile)
 
 ;; TODO: naast prepare ook functies andere kant op: automatisch sql-date-time omzetten naar
 ;; clojure date-time. Maar dit lijkt niet echt nodig.
 (defentity action
-  (entity-fields :id :file_id :create_ts :exec_ts :exec_output :exec_stderr :exec_status
+  (entity-fields :id :create_ts :exec_ts :exec_output :exec_stderr :exec_status
                  :action :fullpath_action :fullpath_other :notes)
+  (belongs-to file {:fk :file_id})
   (prepare (h/updates-in-fn [:id :file_id] to-key 
                             [:create_ts :exec_ts] tc/to-sql-time)))
+
+(defentity author
+  (entity-fields :id :firstname :lastname :fullname :notes)
+  (has-many bookauthor {:fk :author_id})
+  (prepare (h/updates-in-fn [:id] to-key)))
 
 (defentity book
   (entity-fields :id :title :authors :language :edition :npages
                  :pubdate :publisher :isbn10 :isbn13
                  :tags :notes)
+  (has-many bookauthor {:fk :book_id})
+  (has-many bookformat {:fk :book_id})
   (prepare (h/updates-in-fn [:id] to-key :npages to-int
                             [:pubdate] tc/to-sql-time)))
 
+(defentity bookauthor
+  (entity-fields :id :notes)
+  (belongs-to book {:fk :book_id})
+  (belongs-to author {:fk :author_id})
+  (prepare (h/updates-in-fn [:id :book_id :author_id] to-key)))
+
 (defentity bookformat
-  (entity-fields :id :book_id :format :notes)
+  (entity-fields :id :format :notes)
+  (belongs-to book {:fk :book_id})
+  (has-many relfile {:fk :bookformat_id})
   (prepare (h/updates-in-fn [:id :book_id] to-key)))
 
 (defentity directory
   (entity-fields :id :computer :parent_folder :fullpath)
   (belongs-to directory {:fk :parent_id})
   (has-many directory {:fk :parent_id})
+  (has-many file {:fk :directory_id})
   (prepare (h/updates-in-fn [:id :parent_id] to-key)))
 
 (defentity file
   (entity-fields :id :fullpath :filename :folder :filesize :ts :ts_cet
-                 :md5 :directory_id :relfile_id 
-                 :goal :importance :computer :srcbak)
-  (prepare (h/updates-in-fn [:id :directory_id :relfile_id] to-key :filesize to-int
-                            [:ts] tc/to-sql-time)))
+                 :md5 :goal :importance :computer :srcbak)
+  (has-many action {:fk :file_id})
+  (belongs-to directory {:fk :directory_id})
+  (belongs-to relfile {:fk :relfile_id})
+  (prepare (h/updates-in-fn [:id :directory_id :relfile_id] to-key
+                            :filesize to-int
+                            :ts tc/to-sql-time)))
 
 (defentity relfile
-  (entity-fields :id :bookformat_id :relpath :filename :relfolder :filesize :ts :ts_cet
+  (entity-fields :id :relpath :filename :relfolder :filesize :ts :ts_cet
                  :md5 :notes)
+  (belongs-to bookformat {:fk :bookformat_id})
+  (has-many file {:fk :relfile_id})
   (prepare (h/updates-in-fn [:id :bookformat_id] to-key :filesize to-int
                             [:ts] tc/to-sql-time)))
-
-
-
-
 
 ;; TODO alles hieronder een keertje weg.
 
