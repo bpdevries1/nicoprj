7e47e84b8cc9ffe8376bc0c20b19acfbf1355b9e
vreeze42 <nico@ndv.nl>
2016-01-24 11:29:55 +0100

    Add backup cmdline capability

---
+++ mediatools/mediaweb/backupdb.tcl
@@ -0,0 +1,33 @@
+#!/usr/bin/env tclsh86
+
+package require ndv
+
+set log [::ndv::CLogger::new_logger [file tail [info script]] debug]
+
+proc main {argv} {
+  set options {
+    {user.arg "postgres" "Postgres DB user"}
+    {loglevel.arg "" "Set global log level"}
+  }
+  set usage ": [file tail [info script]] \[options]"
+  set dargv [getoptions argv $options $usage]
+  
+  backup_db $dargv    
+}
+
+proc backup_db {dargv} {
+  # set res [exec mysqldump -u $MUSER -h localhost -p$MPASS scheids | gzip -9 > $filename
+  # set backup_root "/media/nas/backups/databases"
+  # set backup_root "/media/nico/data3tb/backups/databases"
+  set backup_root "/media/nico/Iomega HDD/backups/databases/media"
+  file mkdir $backup_root
+  set filename [file join $backup_root "media-[clock format [clock seconds] -format "%Y-%m-%d--%H-%M-%S"].sql"]
+  set res [exec pg_dump -d scheids -U [:user $dargv] -h localhost -f $filename --blobs --clean --create]
+  log info "Result: $res"
+  log info "Backup file: $filename"
+}
+
+main $argv
+
+    
+    
