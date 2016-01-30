60280e62705d1d13b9387c33acd43ccb73867b67
vreeze42 <nico@ndv.nl>
2016-01-30 22:42:04 +0100

    Add books, bookformats and files table to relfile view

--- mediatools/mediaweb/src/mediaweb/models/relfile.clj
+++ mediatools/mediaweb/src/mediaweb/models/relfile.clj
@@ -10,13 +10,30 @@
             [libndv.datetime :refer [parse-date-time]]
             [mediaweb.models.entities :refer :all]))
 
+(def-model-crud :obj-type :relfile
+  :pre-fn (fn [params] (-> params
+                           (h/updates-in [:ts] parse-date-time))))
+
 ;; TODO: hier nu geen flatten in, want later ook refs naar books etc hierbij in een struct.
 (defn relfile-by-id [id]
   (first (select relfile
                  (where {:id (to-int id)}))))
 
-(def-model-crud :obj-type :relfile
-  :pre-fn (fn [params] (-> params
-                           (h/updates-in [:ts] parse-date-time))))
+(defn relfile-books [id]
+  (select relfile
+          (where {:id (to-key id)})
+          (with bookformat
+                (with book
+                      (fields [:id :bid])))))
+
+(defn relfile-bookformats [id]
+  (select relfile
+          (where {:id (to-key id)})
+          (with bookformat
+                (fields [:id :bfid]))))
+
+(defn relfile-files [id]
+  (select file
+          (where {:relfile_id (to-key id)})))
 
 
--- mediatools/mediaweb/src/mediaweb/views/relfile.clj
+++ mediatools/mediaweb/src/mediaweb/views/relfile.clj
@@ -31,10 +31,47 @@
             {:label "MD5" :field :md5 :attrs {:size 32 :readonly true}}
             {:label "Notes" :field :notes :ftype text-area :attrs {:rows 5 :cols 80}}]})
 
+(def-objects-form books-form rf b
+  {:main-type :relfile
+   :row-type :book
+   :model-read-fn mr/relfile-books
+   :actions #{},
+   :columns [{:name "Title", :width 15, :form (book-href (:bid b) (:title b))}
+             {:name "Authors", :width 10, :form (:authors b)}
+             {:name "Pub. date", :width 10, :form (:pubdate b)}
+             {:name "Tags", :width 15, :form (:tags b)}
+             {:name "Notes", :width 40, :form (:notes b)}]})
+
+(def-objects-form bookformats-form rf bf
+  {:main-type :relfile
+   :row-type :bookformat
+   :model-read-fn mr/relfile-bookformats
+   :actions #{}
+   :columns
+   [{:name "Format" :width 30 :form (bookformat-href (:bfid bf) (:format bf))}
+    {:name "Notes"  :width 50 :form (:notes bf)}]})
+
+(def-objects-form files-form rf f
+  {:main-type :relfile
+   :row-type :file
+   :model-read-fn mr/relfile-files
+   :actions #{:delete}
+   :columns
+   [{:name "Filename", :width 15, :form (file-href (:id f) (:filename f))}
+    {:name "Folder", :width 25, :form (:folder f)}
+    {:name "Size", :width 5, :attrs {:align :right}
+     :form (format-filesize (:filesize f))}
+    {:name "Timestamp", :width 20, :attrs {:align :center}
+     :form (format-date-time (:ts f))}
+    {:name "Notes", :width 20, :form (:notes f)}]})
+
 (def-object-page relfile
   {:base-page-fn base-page
    :page-name "Relfile"
-   :parts [{:title "General" :part-fn relfile-form}]
+   :parts [{:title "General" :part-fn relfile-form}
+           {:title "Books" :part-fn books-form}
+           {:title "Formats" :part-fn bookformats-form}
+           {:title "Files" :part-fn files-form}]
    :model-read-fn mr/relfile-by-id
    :name-fn :filename
    :debug true})
