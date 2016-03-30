6441c38ced6b2fb5e4daa9a9507be7bf94b3e323
vreeze42 <nico@ndv.nl>
2016-03-30 21:41:58 +0200

    Cleaned up code

--- mediatools/mediaweb/src/mediaweb/endpoint/itemgroups.clj
+++ mediatools/mediaweb/src/mediaweb/endpoint/itemgroups.clj
@@ -4,12 +4,9 @@
             [mediaweb.views.itemgroup :as vi]))
 
 (h/def-with-default-routes "itemgroup" "itemgroups" "mediaweb.views.itemgroup")
-(h/def-with-default-routes "itemgroupquery" "itemgroupqueries" "mediaweb.views.itemgroup")
+(h/def-with-default-routes "itemgroupquery" "itemgroupqueries"
+  "mediaweb.views.itemgroup")
 (h/def-with-default-routes "member" "members" "mediaweb.views.itemgroup"
-  (GET "/itemgroup-add/:id" [id]
-       (vi/itemgroup-add id {}))
-  (POST "/itemgroup-add/:id" [id & params]
-        (vi/itemgroup-add id params))
   (POST "/itemgroup-add-members/:id" [id & params]
         (vi/itemgroup-add-members id params)))
 
