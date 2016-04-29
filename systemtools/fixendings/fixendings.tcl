db9e4837b45da411c93cf7c2dfa75bd4f865162c
vreeze42 <nico@ndv.nl>
2016-04-29 21:51:22 +0200

    add fix line endings tool

---
+++ systemtools/fixendings/fixendings.tcl
@@ -0,0 +1,68 @@
+#!/usr/bin/env tclsh861
+
+package require ndv
+
+set_log_global debug
+
+proc main {argv} {
+  lassign $argv root
+  handle_dir $root 0
+}
+
+proc handle_dir {dir level} {
+  log debug "handle_dir: $dir"
+  if {$level > 10} {
+    exit
+  }
+  foreach subdir [glob -nocomplain -directory $dir -type d *] {
+    if {![ignore_dir $subdir]} {
+      handle_dir $subdir [expr $level + 1]
+    }
+  }
+  foreach filename [glob -nocomplain -directory $dir -type f *] {
+    handle_file $filename
+  }
+}
+
+proc handle_file {filename} {
+  set tp [det_type $filename]
+  if {$tp == "unix"} {
+    log debug "-> unix: $filename"
+    # exec dos2unix -k $filename
+  } elseif {$tp == "dos"} {
+    log debug "-> dos: $filename"
+    # exec unix2dos -k $filename
+  } elseif {$tp == "bin"} {
+    log debug "binary: $filename"
+  } else {
+    log warn "Unknown: $filename"
+  }
+}
+
+# maybe hidden files auto ignored?
+proc ignore_dir {dir} {
+  if {[file tail $dir] == ".git"} {
+    log warn ".git dir given to ignore_dir: $dir"
+    return 1
+  }
+  return 0
+}
+
+set extensions {
+  unix {.tcl .sql .txt .R .java .clj .py .rb}
+  dos {.bat}
+  ignore {.log}
+}
+
+proc det_type {filename} {
+  global extensions
+  set ext [string tolower [file extension $filename]]
+  foreach {tp exts} $extensions {
+    if {[lsearch $exts $ext]} {
+      return $tp
+    }
+  }
+  return unknown
+}
+
+main $argv
