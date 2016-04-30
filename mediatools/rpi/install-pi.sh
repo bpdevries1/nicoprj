452479d8b88d8ff164f804d7e09085eef796381b
vreeze42 <nico@ndv.nl>
2016-04-30 08:56:24 +0200

    Make mk-symlinks safer

--- mediatools/rpi/install-pi.sh
+++ mediatools/rpi/install-pi.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # scp *.tcl pi@pi:/home/pi/bin
-cp *.tcl /media/nas5tb_1/home-pi/bin
+# cp *.tcl /media/nas5tb_1/home-pi/bin
+unison -auto -batch rpi-bin
+
--- mediatools/rpi/mk-symlinks.tcl
+++ mediatools/rpi/mk-symlinks.tcl
@@ -8,7 +8,8 @@ proc main {argv} {
     exit 1
   }
   if {$link_dir == ""} {
-    set link_dir .
+    # set link_dir .
+    set link_dir "/media/shortcuts-links"
   }
   # make_links $orig_dir $link_dir
   make_links [file normalize $orig_dir] [file normalize $link_dir]
@@ -18,9 +19,19 @@ proc main {argv} {
 # with recursive calls they both go a dir-level deeper.
 proc make_links {orig_dir link_dir} {
   file mkdir $link_dir
+  if {[llength [glob -nocomplain -directory $link_dir *]] == 0} {
+    # ok, empty dir, can be used as target
+  } else {
+    # already dir with files/dirs, create a subdir based on orig_dir in this one.
+    set link_dir [file join $link_dir [file tail $orig_dir]]
+    puts "Creating link_dir as sub dir: $link_dir"
+    file mkdir $link_dir
+  }
+  
   foreach filename [glob -nocomplain -directory $orig_dir -type f *] {
-    file delete [file join $link_dir [file tail $filename]]
-    file link -symbolic [file join $link_dir [file tail $filename]] $filename
+    set link_name [file join $link_dir [file tail $filename]]
+    file delete $link_name
+    file link -symbolic $link_name $filename
   }
   foreach subdir [glob -nocomplain -directory $orig_dir -type d *] {
     make_links $subdir [file join $link_dir [file tail $subdir]]
