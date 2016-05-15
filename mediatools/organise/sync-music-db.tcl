0905209e0a67523b1fb33c7997a1f47702bfb8c6
vreeze42 <nico@ndv.nl>
2016-05-15 17:23:12 +0200

    Check symlinks, real paths and file exists

--- mediatools/organise/sync-music-db.tcl
+++ mediatools/organise/sync-music-db.tcl
@@ -5,7 +5,7 @@
 
 # #!/home/nico/bin/tclsh86
 
-# 13-03-2010 deze lijkt vooral voor albums. Voor singles toch ook, zie upsert_musicfile, regel 155. 
+# 13-03-2010 deze lijkt vooral voor albums. Voor singles toch ook, zie sync_musicfile, regel 155. 
 
 # note: car-player snapt .m4a niet.
 package require ndv
@@ -13,17 +13,18 @@ package require Tclx
 package require struct::list
 
 source ../db/MusicSchemaDef.tcl
-
 source [file join [file dirname [info script]] .. lib setenv-media.tcl]
 source [file join [file dirname [info script]] .. lib libmusic.tcl]
 
-set log [::ndv::CLogger::new_logger [file tail [info script]] info]
+#set log [::ndv::CLogger::new_logger [file tail [info script]] info]
 
 # ::ndv::CLogger::set_log_level_all debug
 # ::ndv::CLogger::set_logfile music.log
-$log set_file music.log
+#$log set_file music.log
 
-proc main {argc argv} {
+set_log_global info
+
+proc main {argv} {
   global log db conn stderr argv0 log SINGLES_ON_SD dryrun
 	$log info "Starting"
 
@@ -51,7 +52,7 @@ proc main {argc argv} {
   # $log debug "encoding: [::mysql::encoding $conn]"
   # TODO vraag of hier ook een utf8 setting nodig is, zou default moeten zijn.
   # ::mysql::exec $conn "set names utf8"
-  $log debug "set names utf8 gedaan"
+  # $log debug "set names utf8 gedaan"
   
   fill_ar_ids
   
@@ -80,6 +81,8 @@ proc repro_error {db} {
   exit 3
 }
 
+# put 4 items in global ar_ids for Artists, Albums, Singles and Musicfiles,
+# which are groups.
 proc fill_ar_ids {} {
   global db conn log ar_ids
   upsert_group Artists
@@ -98,6 +101,8 @@ proc upsert_group {name} {
   }
 }
 
+# Also called recursively
+# [2016-05-15 14:43] TODO: also check which albums are already in the DB, and if they still exist.
 proc handle_filesystem {dir artist_id} {
   global db conn log ar_ids dryrun
 
@@ -119,7 +124,7 @@ proc handle_filesystem {dir artist_id} {
     # $log debug "lst_dir_db: $lst_dir_db"
     # files in this dir
     foreach filename $lst_files {
-      upsert_musicfile $filename $artist_id $album_id lst_dir_db
+      sync_musicfile $filename $artist_id $album_id lst_dir_db
     }
     if {[llength $lst_dir_db] > 0} {
       $log warn "db list not empty yet: $lst_dir_db, probably old items in DB. Remove them"
@@ -135,7 +140,7 @@ proc handle_filesystem {dir artist_id} {
   }
 }
 
-# @result list of files in dir, according to database
+# @result list of files directly in dir (not in subdirs), according to database
 # @note de 'like' geeft ook files in subdirs van deze dir, ongewenst, want ook niet bij filesystem.
 # @note de parent dir wordt niet (altijd) opgeslagen, kan niet veld 'album' gebruiken.
 # @note dus ofwel in de query, ofwel een nabewerking met filter. Met query/regexp kan misschien, maar lijkt lastig, alle tekens moeten dan escaped worden.
@@ -145,7 +150,10 @@ proc det_lst_dir_db {dir} {
   global db conn log ar_ids
   set str_db [$db str_to_db [det_path_in_db $dir]]
   # 10-4-2010 NdV slash toegevoegd aan query, want anders ook files die in parent dir zitten en met dezelfde string beginnen (bv motorcycle).
-  set query "select id, path, generic from musicfile where path like '$str_db/%' order by path"
+  set query "select id, path, generic from musicfile
+             where path like '$str_db/%'
+             and file_exists = 1
+             order by path"
   # 10-4-2010 NdV kan er niet op vertrouwen dat order by altijd dezelfde volgorde kiest als tcl lsort, bv met hoofd en kleine letters. Collating misschien ook anders.
   # volgorde is met deze 'balanced line' essentieel.
   # met filter alle files die in subdirs staan, eruit halen.
@@ -197,24 +205,71 @@ proc upsert_album {dir artist_id} {
       set album_id -2 
     } else {
       set gen_id [$db insert_object generic -gentype "album" -freq 1.0 -play_count 0]
-      if {$artist_id == -1} {
-        set album_id [$db insert_object album -generic $gen_id -path $path_in_db -name [file tail $path_in_db]]
-      } else {
-        set album_id [$db insert_object album -generic $gen_id -path $path_in_db -name [file tail $path_in_db] -artist $artist_id]
+      set artist_part ""
+      if {$artist_id != -1} {
+        set artist_part [list -artist $artist_id]
+      }
+      lassign [det_realpath $dir] realpath is_symlink
+      set album_id [$db insert_object album -generic $gen_id -path $path_in_db \
+                        -name [file tail $path_in_db] {*}$artist_part \
+                        -realpath $realpath -is_symlink $is_symlink]
+      # TODO: onderstaande weg. [2016-05-15 14:18] 
+      if 0 {
+        if {$artist_id == -1} {
+          set album_id [$db insert_object album -generic $gen_id -path $path_in_db -name [file tail $path_in_db]]
+        } else {
+          set album_id [$db insert_object album -generic $gen_id -path $path_in_db -name [file tail $path_in_db] -artist $artist_id]
+        }
       }
       $db insert_object member -mgroup $ar_ids(Albums) -generic $gen_id
     }
   } else {
     # album already found in DB
     set album_id [lindex $lst_ids 0]
+    update_realpath album $album_id $dir
   }    
   return $album_id
+}
+
+# follow symlinks to determine real, absolute path of param path.
+# if path does not exist, return {path -1}
+# could be that one (or more?) of the parent dirs is a symlink, handle this too.
+proc det_realpath {path} {
+  set parts [file split $path]
+  set curpath {}
+  set is_symlink 0
+  foreach part $parts {
+    set prevpath $curpath
+    set curpath [file join $curpath $part]
+    catch {
+      # set curpath [file link $curpath]
+      set curpath [file join $prevpath [file link $curpath]]
+      set is_symlink 1
+    }
+    if {![file exists $curpath]} {
+      return [list $curpath -1]
+    }
+  }
+  return [list $curpath $is_symlink]
+}
+
+# [2016-05-15 16:38] TODO: deze zou later niet meer nodig moeten zijn, als het bij insert steeds gevuld wordt.
+proc update_realpath {table id path} {
+  global db
+  lassign [det_realpath $path] realpath is_symlink
+  set query "update $table set realpath='[$db str_to_db $realpath]',
+             is_symlink=$is_symlink
+             where id=$id
+             and realpath is null"
+  log debug "query: $query"
+  $db exec_query $query
 }
 
 # @param artist_id can be -1, then leave empty in DB.
 # @todo fill trackname and artistname, based on filename? Or keep this in separate script?
 # @note 10-4-2010 use a 'balanced line', compare db-list with filesystem-list.
-proc upsert_musicfile {filename artist_id album_id lst_dir_db_name} {
+# @return id of musicfile, either just inserted or existing one.
+proc sync_musicfile {filename artist_id album_id lst_dir_db_name} {
   global db conn log ar_ids dryrun
   upvar $lst_dir_db_name lst_dir_db
   $log debug "Upserting musicfile: $filename"
@@ -223,6 +278,7 @@ proc upsert_musicfile {filename artist_id album_id lst_dir_db_name} {
   if {$path_in_db == [lindex $lst_dir_db 0 1]} {
     # db en filesys komen overeen, shift db lijst
     set el [::struct::list shift lst_dir_db] ; # remove (pop) first element.
+    update_realpath musicfile [lindex $el 0] $filename
     return [lindex $el 0]
   } else {
     # 2 mogelijkheden: ofwel extra record in db, ofwel nieuwe file in filesystem
@@ -236,10 +292,22 @@ proc upsert_musicfile {filename artist_id album_id lst_dir_db_name} {
           set musicfile_id -2
         } else {
           set gen_id [$db insert_object generic -gentype "musicfile" -freq 1.0 -play_count 0]
-          if {$artist_id == -1} {
-            set musicfile_id [$db insert_object musicfile -generic $gen_id -path $path_in_db -album $album_id]
-          } else {
-            set musicfile_id [$db insert_object musicfile -generic $gen_id -path $path_in_db -album $album_id -artist $artist_id]
+          set artist_part ""
+          if {$artist_id != -1} {
+            set artist_part "-artist $artist_id"
+          }
+          lassign [det_realpath $filename] realpath is_symlink
+          log debug "Insert new musicfile with $realpath and $is_symlink"
+          set musicfile_id [$db insert_object musicfile -generic $gen_id \
+                                -path $path_in_db -album $album_id {*}$artist_part \
+                                -realpath $realpath -is_symlink $is_symlink]
+          # TODO: remove if 0 below:
+          if 0 {
+            if {$artist_id == -1} {
+              set musicfile_id [$db insert_object musicfile -generic $gen_id -path $path_in_db -album $album_id]
+            } else {
+              set musicfile_id [$db insert_object musicfile -generic $gen_id -path $path_in_db -album $album_id -artist $artist_id]
+            }
           }
           $db insert_object member -mgroup $ar_ids(Musicfiles) -generic $gen_id
           if {[regexp "/Music/Singles/" $filename]} {
@@ -260,14 +328,29 @@ proc upsert_musicfile {filename artist_id album_id lst_dir_db_name} {
       # db lijst aanpassen en opnieuw proberen.
       ::struct::list shift lst_dir_db
       # 29-9-2010 blijkbaar recursief opnieuw proberen.
-      return [upsert_musicfile $filename $artist_id $album_id lst_dir_db]
+      return [sync_musicfile $filename $artist_id $album_id lst_dir_db]
       # return $musicfile_id      
     }
   }
   return $musicfile_id
 }
 
+# [2016-05-15 14:49] Only mark record as deleted, don't delete.
+# reason: file might be moved, and played info is still interesting.
+# this one still specific for musicfile now.
 proc remove_db_record {db_record} {
+  global conn log dryrun
+  lassign $db_record id path generic
+  set query "update musicfile set file_exists = 0 where id = $id"
+  $log debug "remove_db_record: $db_record"
+  if {$dryrun} {
+    $log info "Dryrun, don't exec: $query"
+  } else {
+    pg_query $conn $query
+  }
+}
+
+proc remove_db_record_old {db_record} {
   global conn log dryrun
   lassign $db_record id path generic
   # TODO check of generic record ook gedelete wordt (bv via cascading delete)
@@ -285,4 +368,5 @@ proc remove_db_record {db_record} {
   }  
 }
 
-main $argc $argv
+main $argv
+
