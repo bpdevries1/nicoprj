7bc0433fe0847a9d391c7256c9cf86ccce03a9fd
vreeze42 <nico@ndv.nl>
2016-05-26 22:21:48 +0200

    Add showcase and batchfiles to jmeter distr testing

---
+++ perftools/jmeter/distributed-testing/setenv-server.bat
@@ -0,0 +1,6 @@
+set JAVA_HOME=c:\nico\java\jdk1.5.0
+set JAVA_ROOT=c:\nico\java
+set JMETER_BIN=c:\nico\java\jmeter23rc3-src\jakarta-jmeter-2.3RC3\bin\
+set JMETER_HOME=c:\nico\java\jmeter23rc3-src\jakarta-jmeter-2.3RC3
+
+
