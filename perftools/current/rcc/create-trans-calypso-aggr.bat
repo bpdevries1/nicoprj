bc7a01ee162710af4f4b5e4acd38a96c7c80cf75
vreeze42 <nico@ndv.nl>
2016-06-03 09:40:24 +0200

    Sql for creating test phases within testrun.

---
+++ perftools/current/rcc/create-trans-calypso-aggr.bat
@@ -0,0 +1,3 @@
+rem tclsh c:\pcc\nico\nicoprj\dbtools\dbscript.tcl -db C:\PCC\Nico\Testruns\RCC-All\vugen-20160526b-intl-varying-latency-nconc6-nocdn.db -script create-trans-lat-aggr.sql
+tclsh c:\pcc\nico\nicoprj\dbtools\dbscript.tcl -db C:\PCC\Nico\Testruns\Calypso\max30.db -script create-trans-calypso-aggr.sql
+tclsh c:\pcc\nico\nicoprj\dbtools\dbscript.tcl -db C:\PCC\Nico\Testruns\Calypso\max50.db -script create-trans-calypso-aggr.sql
---
+++ perftools/current/rcc/create-trans-lat-aggr.bat
@@ -0,0 +1,3 @@
+rem tclsh c:\pcc\nico\nicoprj\dbtools\dbscript.tcl -db C:\PCC\Nico\Testruns\RCC-All\vugen-20160526b-intl-varying-latency-nconc6-nocdn.db -script create-trans-lat-aggr.sql
+tclsh c:\pcc\nico\nicoprj\dbtools\dbscript.tcl -db C:\PCC\Nico\Testruns\RCC-All\vugen-20160526a-intl-varying-latency-nconc6.db -script create-trans-lat-aggr.sql
+tclsh c:\pcc\nico\nicoprj\dbtools\dbscript.tcl -db C:\PCC\Nico\Testruns\RCC-All\vugen-20160525h-intl-varying-latency-nconc6.db -script create-trans-lat-aggr.sql
