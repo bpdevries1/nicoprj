16887e12f7aa26d3327ab9e3008fe3add4467f80
vreeze42 <nico@ndv.nl>
2016-06-29 21:35:46 +0200

    add read-vuserlogs-db-dir.sh

---
+++ perftools/loadrunner/vugentools/vuserlog/read-vuserlogs-db-dir.sh
@@ -0,0 +1,11 @@
+#rem [2016-04-20 10:49:24] now just specify a dir and create DB's for all subdirs where no DB exists yet.
+
+#rem even voor bouwen/debuggen
+#rem del C:\pcc\Nico\Testruns\RCC-All\run596.db
+
+./read-vuserlogs-db-dir.tcl -dir /home/ymor/RABO/testruns/ClientReporting
+# tclsh read-vuserlogs-db-dir.tcl -dir C:\pcc\Nico\Testruns\clientreporting
+#tclsh read-vuserlogs-db-dir.tcl -dir C:\pcc\Nico\Testruns\Calypso
+
+
+C:\pcc\Nico\Testruns\RCC-All
