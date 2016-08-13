dc9bb2af6171ede848b5683034933b708fbd17fe
vreeze42 <nico@ndv.nl>
2016-08-13 11:52:48 +0200

    AHK log->report, first version

--- perftools/loadrunner/buildtool/ahk/report.tcl
+++ perftools/loadrunner/buildtool/ahk/report.tcl
@@ -1,15 +1,14 @@
 # TODO: need to put this setting/location somewhere. This could be a user
 # setting in ~/.config/buildtool, next to location of eg R binary.
-ndv::source_once ../../vugentools/vuserlog/read-vuserlogs-db.tcl
+ndv::source_once ../../../autohotkey/ahklog/read-ahklogs-db.tcl
 ndv::source_once ../../vugentools/vuser-report/vuser-report.tcl
 
 task report {Create report of output.txt in script dir
-  Copy output.txt to testruns dir, call vugentools/vuserlog/read-vuserlogs-db.tcl
-  and create Html report.
+  Copy output.txt to testruns dir, call perftools/autohotkey/ahklog/read-ahklogs-db.tcl
+  and create html report.
 } {{summary "Create summary report, with aggregate times and errors"}
   {full "Create full report, with each iteration/transaction."}
   {all "Both summary and full"}
-  {ssl "Read SSL logs into DB"}
 } {
   global testruns_dir
   if {[regexp {<FILL IN>} $testruns_dir]} {
@@ -19,22 +18,23 @@ task report {Create report of output.txt in script dir
   # opt available
 
   # first copy output.txt to restruns dirs, iff not already done.
-  if {![file exists output.txt]} {
+  set logfilename output2/logfile.txt
+  if {![file exists $logfilename]} {
     puts "WARN: no output.txt found"
     return
   }
   set subdir [file join $testruns_dir \
-                  "vugen-[clock format [file mtime output.txt] -format \
+                  "ahk-[clock format [file mtime $logfilename] -format \
                   "%Y-%m-%d--%H-%M-%S"]"]
-  set to_file [file join $subdir output.txt]
+  set to_file [file join $subdir [file tail $logfilename]]
   if {![file exists $to_file]} {
     file mkdir $subdir
-    file copy output.txt $to_file
+    file copy $logfilename $to_file
   }
 
   # then call read_logfile_dir; idempotency should already be arranged by read_logfile_dir
-  set dbname [file join $subdir "vuserlog.db"]
-  read_logfile_dir $subdir $dbname [:ssl $opt] split_transname
+  set dbname [file join $subdir "ahklog.db"]
+  read_logfile_dir $subdir $dbname
 
   # and finally make the report.
   vuser_report $subdir $dbname $opt
