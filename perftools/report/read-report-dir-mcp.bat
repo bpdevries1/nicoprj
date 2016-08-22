d4da377e0bca924b4a1ed9c8fad88357bbde332a
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2016-08-22 17:17:41 +0200

    Flexfields - integer

--- perftools/report/read-report-dir-mcp.bat
+++ perftools/report/read-report-dir-mcp.bat
@@ -3,4 +3,4 @@ rem tclsh read-report-dir.tcl -all -dir c:/PCC/Nico/Testruns/MCPv3
 
 rem even alleen run666
 rem tclsh read-report-dir.tcl -all -dir c:/PCC/Nico/Testruns/MCPv3/run667
-tclsh read-report-dir.tcl -debug -dir c:/PCC/Nico/Testruns/MCPv3/run667
+tclsh read-report-dir.tcl -debug -dir c:/PCC/Nico/Testruns/MCPv3/run670
