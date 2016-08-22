cf78684686695b5510f519bfb35a72be4fd28ac5
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2016-08-22 14:17:53 +0200

    Add flexfields table

---
+++ perftools/report/read-report-dir-mcp.bat
@@ -0,0 +1,6 @@
+rem for all subdirs in dir: read logs into sqlite db and create html reports.
+rem tclsh read-report-dir.tcl -all -dir c:/PCC/Nico/Testruns/MCPv3
+
+rem even alleen run666
+rem tclsh read-report-dir.tcl -all -dir c:/PCC/Nico/Testruns/MCPv3/run667
+tclsh read-report-dir.tcl -debug -dir c:/PCC/Nico/Testruns/MCPv3/run667
