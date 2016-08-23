c98d6eb0ea1b9dde3725d6ea71bf2ce1b82105d3
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2016-08-23 17:21:17 +0200

    Add trans params for Vugen scripts

--- perftools/report/read-report-dir-mcp.bat
+++ perftools/report/read-report-dir-mcp.bat
@@ -3,4 +3,7 @@ rem tclsh read-report-dir.tcl -all -dir c:/PCC/Nico/Testruns/MCPv3
 
 rem even alleen run666
 rem tclsh read-report-dir.tcl -all -dir c:/PCC/Nico/Testruns/MCPv3/run667
-tclsh read-report-dir.tcl -debug -dir c:/PCC/Nico/Testruns/MCPv3/run670
+rem tclsh read-report-dir.tcl -all -dir c:/PCC/Nico/Testruns/MCPv3/run670
+rem tclsh read-report-dir.tcl -all -dir c:/PCC/Nico/Testruns/MCPv3/ahk-upload-21
+rem tclsh read-report-dir.tcl -dir c:/PCC/Nico/Testruns/MCPv3/vugen-2016-08-23--10-36-11
+tclsh read-report-dir.tcl -all -debug -dir c:/PCC/Nico/Testruns/MCPv3/vugen-2016-08-23--14-26-23
