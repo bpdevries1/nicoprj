27ea612ac370a1e6ed751ed3a0f286b9bdc94e24
vreeze42 <nico@ndv.nl>
2016-09-24 10:22:56 +0200

    Add sourcedep task

---
+++ buildtool/vugen/sourcedep.tcl
@@ -0,0 +1,14 @@
+set perftools_dir [file normalize [file join \
+                                       [file dirname [info script]] .. .. perftools]]
+source [file join $perftools_dir .. devtools sourcedep sourcedep.tcl]
+
+task sourcedep {Create source dependency graphs/html
+} {{rootdir.arg "." "Root directory"}
+  {dirs.arg "" "Subdirs within root dir to handle, empty for all (: separated)"}
+  {targetdir.arg "sourcedep" "Directory where to generate DB, images, html"}
+  {db.arg "sourcedep.db" "SQLite DB to create, relative to target dir"}
+  {deletedb "Delete DB first"}
+  {loglevel.arg "info" "Set loglevel"}  
+} {
+  sourcedep $opt
+}
