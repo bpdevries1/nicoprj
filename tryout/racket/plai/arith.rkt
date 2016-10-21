6ea3be97267a937ed9823eec3d277ab57651d6f9
vreeze42 <nico@ndv.nl>
2016-10-21 16:46:51 +0200

    Add unary minus

--- tryout/racket/plai/arith.rkt
+++ tryout/racket/plai/arith.rkt
@@ -6,18 +6,6 @@
   [plusC (l : ArithC) (r : ArithC)]
   [multC (l : ArithC) (r : ArithC)])
 
-;; [2016-10-21 14:58] parse nu naar ArithS, dus deze weg.
-#;(define (parse [s : s-expression]) : ArithC
-  (cond
-    [(s-exp-number? s) (numC (s-exp->number s))]
-    [(s-exp-list? s)
-     (let ([sl (s-exp->list s)])
-       (case (s-exp->symbol (first sl))
-         [(+) (plusC (parse (second sl)) (parse (third sl)))]
-         [(*) (multC (parse (second sl)) (parse (third sl)))]
-         [else (error 'parse "invalid list input")]))]
-    [else (error 'parse "invalid input")]))
-
 ;; [2016-10-21 13:34] ok, dit werkt hierboven, maar BR manier lijkt handiger, met een grammar.
 (define (interp [a : ArithC]) : number
   (type-case ArithC a
@@ -25,12 +13,12 @@
     [plusC (l r) (+ (interp l) (interp r))]
     [multC (l r) (* (interp l) (interp r))]))
 
-
 ;; [2016-10-21 14:55] Surface syntax, tegenhanger van Core. Deze met minus.
 (define-type ArithS
   [numS (n : number)]
   [plusS (l : ArithS) (r : ArithS)]
   [bminusS (l : ArithS) (r : ArithS)]
+  [uminusS (l : ArithS)]
   [multS (l : ArithS) (r : ArithS)])
 
 ;; idee is dan van s-expr via Surface naar Core datatype.
@@ -46,37 +34,12 @@
      (let ([sl (s-exp->list s)])
        (case (s-exp->symbol (first sl))
          [(+) (plusS (parse (second sl)) (parse (third sl)))]
-         [(-) (bminusS (parse (second sl)) (parse (third sl)))]
+         [(-) (if (= 3 (length sl))
+                  (bminusS (parse (second sl)) (parse (third sl)))
+                  (uminusS (parse (second sl))))]
          [(*) (multS (parse (second sl)) (parse (third sl)))]
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")]))
-
-;; TODO: desugar. Moet wel alles langs en omzetten van S->C.
-;; eerst via cond, later ook type case, pattern match?
-#;(define (desugar [s : ArithS]) : ArithC
-  (cond
-    [(numS? s) (numC (numS-n s))]
-    [(plusS? s) (plusC (desugar (plusS-l s)) (desugar (plusS-r s)))]
-    [(bminusS? s) (plusC (desugar (bminusS-l s))
-                        (multC (numC -1) (desugar (bminusS-r s))))]
-    [(multS? s) (multC (desugar (multS-l s)) (desugar (multS-r s)))]
-    [else (error 'desugar42 "invalid input")]))
-
-;; [2016-10-21 13:50] Test cases for interpreter
-;; en moeten via ArithS nog steeds werken.
-;; threading (->) operator zou hier ook handig zijn.
-#|
-(test (interp (desugar (parse '0))) 0)
-(test (interp (desugar (parse '23))) 23)
-(test (interp (desugar (parse '(+ 1 2)))) 3)
-(test (interp (desugar (parse '(* 3 2)))) 6)
-(test (interp (desugar (parse '(+ (* 3 4) (+ 2 3))))) 17)
-(test (interp (desugar (parse '(+ 1.0 2.5)))) 3.5)
-(test (interp (desugar (parse '(+ 1/4 1/3)))) 7/12)
-
-(test (interp (desugar (parse '(- 8 6)))) 2)
-(test (interp (desugar (parse '(- 8 (* 2 3))))) 2)
-|#
 
 ;; [2016-10-21 15:51] ok, dit werkt. Dan ook met type case / pattern match.
 ;; [2016-10-21 16:06] grappig: else case is unreachable nu.
@@ -85,7 +48,8 @@
     [numS (n) (numC n)]
     [plusS (l r) (plusC (desugar l) (desugar r))]
     [multS (l r) (multC (desugar l) (desugar r))]
-    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]))
+    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
+    [uminusS (l) (multC (numC -1) (desugar l))]))
 
 ;; [2016-10-21 16:21] ook unary minus erbij:
 (test (interp (desugar (parse '0))) 0)
@@ -97,6 +61,6 @@
 (test (interp (desugar (parse '(+ 1/4 1/3)))) 7/12)
 (test (interp (desugar (parse '(- 8 6)))) 2)
 (test (interp (desugar (parse '(- 8 (* 2 3))))) 2)
-;; (test (interp (desugar (parse '(- 1)))) -1)
+(test (interp (desugar (parse '(- 1)))) -1)
 
     
