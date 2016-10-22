120fd596e1a6524d16d076557768d667e089bc73
vreeze42 <nico@ndv.nl>
2016-10-22 20:53:58 +0200

    Interp - use env instead of subst

--- tryout/racket/plai/arith.rkt
+++ tryout/racket/plai/arith.rkt
@@ -20,6 +20,12 @@
   [fdC (name : symbol) (arg : symbol) (body : ExprC)])
 
 ;; (fdC 'double 'x (plusC (idC 'x) (idC 'x)))
+(define-type Binding
+  [bind (name : symbol) (val : number)])
+
+(define-type-alias Env (listof Binding))
+(define mt-env empty)
+(define extend-env cons)
 
 (define-type ExprS
   [numS (n : number)]
@@ -33,7 +39,7 @@
 ;; [2016-10-21 19:57] for now return the double function.
 ;; [2016-10-21 20:18] deze def ging goed, ook al is 'ie niet typed.
 #;(define (get-fundef name lofundefs)
-  (fdC 'double 'x (plusC (idC 'x) (idC 'x))))
+    (fdC 'double 'x (plusC (idC 'x) (idC 'x))))
 
 ;; [2016-10-21 20:21] had gehoopt dat dit met iets meer higher-level functie kon:
 ;; bv direct met een hashmap.
@@ -51,39 +57,61 @@
 ;; de identifier niet subst-ed is. Kan nu een error geven, kijken wat 'ie doet.
 ;; [2016-10-21 21:49] Door inter-subst bij appC wordt eager application gedaan.
 #;(define (interp [e : ExprC] [fds : (listof FunDefC)]) : number
-  (type-case ExprC e
-    [numC (n) n]
-    ;; <idC-interp-case>
-    ;; [2016-10-21 20:17] zelf wel goed verzonnen, moet idd fout gaan!
-    [idC (s) (error 'id "Unbound identifier")]
-    [appC (f a) (local ([define fd (get-fundef f fds)])
-                  (interp (subst (interp a fds)
-                                 (fdC-arg fd)
-                                 (fdC-body fd))
-                          fds))]
-    [plusC (l r) (+ (interp l fds) (interp r fds))]
-    [multC (l r) (* (interp l fds) (interp r fds))]
-    [eqC (l r) (if (= (interp l fds) (interp r fds)) 1 0)]
-    [ifC (c t e) (if (= 0 (interp c fds)) (interp e fds) (interp t fds))]))
+    (type-case ExprC e
+      [numC (n) n]
+      ;; <idC-interp-case>
+      ;; [2016-10-21 20:17] zelf wel goed verzonnen, moet idd fout gaan!
+      [idC (s) (error 'id "Unbound identifier")]
+      [appC (f a) (local ([define fd (get-fundef f fds)])
+                    (interp (subst (interp a fds)
+                                   (fdC-arg fd)
+                                   (fdC-body fd))
+                            fds))]
+      [plusC (l r) (+ (interp l fds) (interp r fds))]
+      [multC (l r) (* (interp l fds) (interp r fds))]
+      [eqC (l r) (if (= (interp l fds) (interp r fds)) 1 0)]
+      [ifC (c t e) (if (= 0 (interp c fds)) (interp e fds) (interp t fds))]))
 
-;; in appC (begin (define)) gebruiken -> define not allowed in expression context.
-;; met let -> werkt prima zo te zien.
-;; mss moet je local gebruiken zodat je niet (meer) in expression context zit.
-(define (interp [e : ExprC] [fds : (listof FunDefC)]) : number
-  (type-case ExprC e
+;; [2016-10-22 19:50] nieuwe interp met gebruik van Env (=listof Binding)
+(define (interp [expr : ExprC] [env : Env] [fds : (listof FunDefC)]) : number
+  (type-case ExprC expr
     [numC (n) n]
-    ;; <idC-interp-case>
-    ;; [2016-10-21 20:17] zelf wel goed verzonnen, moet idd fout gaan!
-    [idC (s) (error 'id "Unbound identifier")]
-    [appC (f a) (let ([fd (get-fundef f fds)])
-                  (interp (subst (interp a fds)
-                                 (fdC-arg fd)
-                                 (fdC-body fd))
+    [idC (n) (lookup n env)]
+    ;; deferral of substitution betekent hier in de env zetten.
+    [appC (f a) (local ([define fd (get-fundef f fds)])
+                  ; (interp <body> (extend-env (bind <arg> < a of (interp a)>)) fds
+                  ; a is ExprC, dus ook hier interp nodig.
+                  ; bij functie applicatie weer met empty env beginnen.
+                  (interp (fdC-body fd)
+                          (extend-env (bind (fdC-arg fd)
+                                            (interp a env fds))
+                                      mt-env)
                           fds))]
-    [plusC (l r) (+ (interp l fds) (interp r fds))]
-    [multC (l r) (* (interp l fds) (interp r fds))]
-    [eqC (l r) (if (= (interp l fds) (interp r fds)) 1 0)]
-    [ifC (c t e) (if (= 0 (interp c fds)) (interp e fds) (interp t fds))]))
+    
+    [plusC (l r) (+ (interp l env fds) (interp r env fds))]
+    [multC (l r) (* (interp l env fds) (interp r env fds))]
+    [eqC (l r) (if (= (interp l env fds) (interp r env fds)) 1 0)]
+    [ifC (c t e) (if (= 0 (interp c env fds)) (interp e env fds) (interp t env fds))]))
+
+;; [2016-10-22 20:08] deze zelf bedacht, nog testen dus. Straks eerst compleet,
+;; met de testcases. Mocht er iets fout gaan, dan losse testen maken eerst.
+;; ik zocht net case, maar had cond moeten zoeken.
+;; had equal? maar symbol=? is specifieker.
+(define (lookup (n : symbol) (env : Env)) : number
+  (if (empty? env)
+      (error 'lookup "Cannot find symbol in env")
+      (if (symbol=? n (bind-name (first env)))
+          (bind-val (first env))
+          (lookup n (rest env)))))
+
+;; [2016-10-22 20:21] de officele hieronder:
+#;(define (lookup [for : symbol] [env : Env]) : number
+    (cond
+      [(empty? env) (error 'lookup "name not found")]
+      [else (cond
+              [(symbol=? for (bind-name (first env)))
+               (bind-val (first env))]
+              [else (lookup for (rest env))])]))
 
 
 ;; [2016-10-21 19:09] helper function, waar is * voor? Of beetje CT achtig, dat je
@@ -179,12 +207,14 @@ three example functions above. What should it produce?
 
 
 ;; [2016-10-21 16:21] ook unary minus erbij:
-(test (interp (desugar (parse '0)) (list)) 0)
+;; [2016-10-22 20:20] doet het nu even niet meer, nu geen focus op desugar en parse.
+;; (test (interp (desugar (parse '0)) (list)) 0)
 
 ;; [2016-10-21 20:10] desugar en parse moeten nog bijgewerkt worden.
 ;; dus eerst direct ExprC variant.
 
 ;; [2016-10-21 22:18] zowaar mogelijk hiermee een werkende fac functie te maken!
+;; ook in 1x goed, hoefde niet te debuggen.
 (define fds (list (fdC 'double 'x (plusC (idC 'x) (idC 'x)))
                   (fdC 'fac 'x (ifC (eqC (idC 'x) (numC 1))
                                     (numC 1)
@@ -193,13 +223,49 @@ three example functions above. What should it produce?
                                            (appC 'fac (plusC (numC -1)
                                                              (idC 'x))))))))
 
-(test (interp (appC 'double (numC 5)) fds) 10)
-(test (interp (appC 'double (plusC (numC 2) (numC 4))) fds) 12)
+(test (interp (appC 'double (numC 5)) mt-env fds) 10)
+(test (interp (appC 'double (plusC (numC 2) (numC 4))) mt-env fds) 12)
 
-(test (interp (appC 'fac (numC 1)) fds) 1)
-(test (interp (appC 'fac (numC 2)) fds) 2)
-(test (interp (appC 'fac (numC 3)) fds) 6)
-(test (interp (appC 'fac (numC 6)) fds) 720)
+(test (interp (appC 'fac (numC 1)) mt-env fds) 1)
+(test (interp (appC 'fac (numC 2)) mt-env fds) 2)
+(test (interp (appC 'fac (numC 3)) mt-env fds) 6)
+(test (interp (appC 'fac (numC 6)) mt-env fds) 720)
+
+;; [2016-10-22 20:25] dan nog een paar test cases uit het boek:
+;; nu ook duidelijk waarom het mt-env heet, mt uitspreken klinkt als empty.
+(test (interp (plusC (numC 10) (appC 'const5 (numC 10)))
+              mt-env
+              (list (fdC 'const5 '_ (numC 5))))
+      15)
+
+(test (interp (plusC (numC 10) (appC 'double (plusC (numC 1) (numC 2))))
+              mt-env
+              (list (fdC 'double 'x (plusC (idC 'x) (idC 'x)))))
+      16)
+
+(test (interp (plusC (numC 10) (appC 'quadruple (plusC (numC 1) (numC 2))))
+              mt-env
+              (list (fdC 'quadruple 'x (appC 'double (appC 'double (idC 'x))))
+                    (fdC 'double 'x (plusC (idC 'x) (idC 'x)))))
+      22)
+
+;; [2016-10-22 20:35] Hier komt idd 7 uit, vraag of dit goed is, of dat een foutmelding
+;; gegeven had moeten worden.
+;; [2016-10-22 20:42] deze levert nu idd een fout op, doordat je steeds met een mt-env
+;; begint.
+#;(interp (appC 'f1 (numC 3))
+        mt-env
+        (list (fdC 'f1 'x (appC 'f2 (numC 4)))
+              (fdC 'f2 'y (plusC (idC 'x) (idC 'y)))))
+
+
+;; [2016-10-22 20:30] vraag dan is: spot the bug. Kan me iets voorstellen bij namen
+;; door elkaar halen, mss env weer kleiner maken als een appl klaar is? Of als je op
+;; diverse niveau's een 'x hebt, hoewel shadowing wel lijkt te werken: nieuwe binding
+;; wordt aan het begin toegevoegd, dus als eerste gevonden, lijkt goed.
+;; voorbeeld gaat richting closure, vraag of het goed is. Over te discussieren,
+;; waarsch niet.
+;; oplossing? mss nested env's, is wel de officiele manier die ik eerder heb gezien.
 
 ;; deze gaat waarsch een stack overflow opleveren.
 ;; maar het duurt wel even...
