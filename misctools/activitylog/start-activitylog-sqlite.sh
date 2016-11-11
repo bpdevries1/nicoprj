3069e6be3d4b21d148542498f4fe98f9dbeb6c40
vreeze42 <nico@ndv.nl>
2016-11-11 22:45:25 +0100

    add nohup

--- misctools/activitylog/start-activitylog-sqlite.sh
+++ misctools/activitylog/start-activitylog-sqlite.sh
@@ -1,3 +1,3 @@
-/home/nico/nicoprj/misctools/activitylog/activitylog-sqlite.tcl -l /home/nico/log/activ &
+nohup /home/nico/nicoprj/misctools/activitylog/activitylog-sqlite.tcl -l /home/nico/log/activitylog/activ &
 # /home/nico/nicoprj/misctools/activitylog/activitylog.tcl -l /home/nico/projecten/activitylog/activ.tsv -i 5 &
 
