fccbcc435916a91760cb8e97c9feb712dd1e94f9
vreeze42 <nico@ndv.nl>
2016-12-11 21:57:27 +0100

    only check on windows, never for now.

--- tryout/tcl-odbc/test-odbc.tcl
+++ tryout/tcl-odbc/test-odbc.tcl
@@ -1,6 +1,9 @@
 package require tdbc
 package require tdbc::odbc
 
+# [2016-12-11 21:56] don't test for now, should check iff windows.
+#@test never
+
 set db [tdbc::odbc::connection create db2 "Driver=SQL Server Native Client 11.0;Server=LAPTOP-NICO\\SQLEXPRESS;Database=testndv2;Trusted_Connection=yes;"]
 set s [$db prepare "select * from testtb2"]
 puts [$s allrows]
