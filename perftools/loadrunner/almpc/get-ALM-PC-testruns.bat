dd2553f1623c9749a69004fe85b35d62a5633455
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2016-12-22 16:44:52 +0100

    Use get-ALM-PC-testruns.bat for RRS.

--- perftools/loadrunner/almpc/get-ALM-PC-testruns.bat
+++ perftools/loadrunner/almpc/get-ALM-PC-testruns.bat
@@ -2,13 +2,13 @@
 setlocal
 
 rem eerst shared env, is default:
-tclsh get-ALM-PC-testruns.tcl
-tclsh get-ALM-PC-testruns.tcl -domain PCC -project WeekvanWaarden
+tclsh get-ALM-PC-testruns.tcl -firstrunid 731 -lastrunid 731
+rem tclsh get-ALM-PC-testruns.tcl -domain PCC -project WeekvanWaarden
 
 rem volgenden lukken niet met Runs download, waarschijnlijk te oud.
 rem calypso vanaf 49 (t/m 111)
-tclsh get-ALM-PC-testruns.tcl -domain RI -project Calypso_HW -firstrunid 49 -lastrunid 115
+rem tclsh get-ALM-PC-testruns.tcl -domain RI -project Calypso_HW -firstrunid 49 -lastrunid 115
 
-tclsh get-ALM-PC-testruns.tcl -domain RI -project RI_Combitest -firstrunid 1105 -lastrunid 1650
+rem tclsh get-ALM-PC-testruns.tcl -domain RI -project RI_Combitest -firstrunid 1105 -lastrunid 1650
 
 
