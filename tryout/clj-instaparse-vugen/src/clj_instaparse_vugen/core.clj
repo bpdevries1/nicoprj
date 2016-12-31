6e71ef7effa0b1d05c741882e5dcab18c6ae0591
vreeze42 <nico@ndv.nl>
2016-12-31 18:08:53 +0100

    check

--- tryout/clj-instaparse-vugen/src/clj_instaparse_vugen/core.clj
+++ tryout/clj-instaparse-vugen/src/clj_instaparse_vugen/core.clj
@@ -22,17 +22,40 @@
 (def mini-text (slurp (io/resource "mini.usr")))
 (def mini (ini-parse mini-text))
 
-(def c-parser (-> "clang.ebnf" io/resource slurp insta/parser))
+#_(def c-parser (-> "clang.ebnf" io/resource slurp (insta/parser :auto-whitespace :standard)))
+
+(def c-grammar (-> "clang.ebnf" io/resource slurp))
+(def c-parser (insta/parser c-grammar :auto-whitespace :standard))
 
 (def vuser-end-text (slurp (io/resource "vuser_end.c")))
 
 (defn measured-parse
   "Measure time and number of parse trees"
   [parser text]
+  (println "First tree:")
+  (time (insta/parse parser text))
+  (println "All trees:")
   (let [trees (time (insta/parses parser text :total true :unhide :all))]
     (println "Number of characters:" (count text))
     (println "Number of trees:" (count trees))
     trees))
+
+(defn measured-parse-file
+  "Measured parse of a file, see measured-parse"
+  [parser filename]
+  (println "Filename:" filename)
+  (measured-parse parser (slurp (io/resource filename))))
+
+(defn pprint-file
+  "pretty print part of trees to filename"
+  [trees ndx basename]
+  (pprint (nth trees ndx) (io/writer (str "/tmp/"  basename "-" ndx ".txt"))))
+
+(defn pprint-file-trees
+  "Print all trees of parse to a separate file"
+  [trees basename]
+  (doseq [ndx (range (count trees))]
+    (pprint-file trees ndx basename)))
 
 ;; total true -> embed failure node in tree.
 (time (def vuser-end (c-parser vuser-end-text :total true :unhide :all)))
@@ -41,8 +64,23 @@
 (def vuser-ends (measured-parse c-parser vuser-end-text))
 
 (def landing-text (slurp (io/resource "landing.c")))
+(def revert-text (slurp (io/resource "revert.c")))
 ;; total true -> embed failure node in tree.
 #_(def landing (c-parser landing-text :total true :unhide :all))
+
+#_(def landing-2 (measured-parse-file c-parser "landing-2.c"))
+#_(def landing-4 (measured-parse-file c-parser "landing-4.c"))
+#_(def landing-19 (measured-parse-file c-parser "landing-19.c"))
+#_(def landing-35 (measured-parse-file c-parser "landing-35.c"))
+#_(def landing-75 (measured-parse-file c-parser "landing-75.c"))
+#_(def landing-78 (measured-parse-file c-parser "landing-78.c"))
+#_(def landing-86 (measured-parse-file c-parser "landing-86.c"))
+#_(def landing-89 (measured-parse-file c-parser "landing-89.c"))
+#_(def landing-95 (measured-parse-file c-parser "landing-95.c"))
+#_(def landing-104 (measured-parse-file c-parser "landing-104.c"))
+#_(def landing-106 (measured-parse-file c-parser "landing-106.c"))
+#_(def landing (measured-parse-file c-parser "landing.c"))
+(def landing (measured-parse-file c-parser "revert.c"))
 
 ;; (clojure.pprint/pprint *map* (clojure.java.io/writer "foo.txt"))
 
