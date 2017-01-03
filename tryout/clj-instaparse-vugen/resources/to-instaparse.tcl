a141ff632bd444de2560607b4a93bff821d3c7d3
vreeze42 <nico@ndv.nl>
2017-01-03 22:19:02 +0100

    Start adding <ows> rules back again

--- tryout/clj-instaparse-vugen/resources/to-instaparse.tcl
+++ tryout/clj-instaparse-vugen/resources/to-instaparse.tcl
@@ -5,6 +5,8 @@ package require struct::set
 
 use libfp
 
+set OWS 1
+
 proc main {argv} {
   lassign $argv until_line
   set infile "clang-orig.ebnf"
@@ -15,11 +17,19 @@ proc main {argv} {
 }
 
 # from http://tigcc.ticalc.org/doc/keywords.html
-set keywords { auto break case char const continue default do double else enum extern float for goto if int long register return short signed sizeof static struct switch typedef union unsigned void volatile while 
+set keywords { auto break case char const continue default do double else enum extern float for goto if int long register return short signed sizeof static struct switch typedef union unsigned void volatile while thread_local bool complex imaginary atomic
 }
 
+# if keywords or operators are already included in quotes, the <ows> won't get added. Fixed by first adding quotes, next add <ows> after each quoted element. 
+# TODO: ows rule should include comments (as now done with auto-whitespace)
+# TODO: add <ows> after usage of terminals like identifier, don't want the ows to be part of identifiers, constants and strings.
 proc transform {text until_line} {
-  global keywords
+  global keywords OWS
+  if {$OWS} {
+    set ows " <ows>"
+  } else {
+    set ows ""
+  }
   # breakpoint
   # don't include |, is used in BNF itself.
   foreach chars {, : * ? \{ \} || << >> ^ / % ++ -- -> = == > < >= <= + - . ~ ! *= /= += -= <<= >>= ^= != |= %= ... ; ( ) [ ]} {
@@ -34,6 +44,61 @@ proc transform {text until_line} {
     regsub -all " ${keyword}(\[ \\n\])" $text " '$keyword'\\1" text
   }
 
+  # next add <ows> after each terminal symbol/operator/keyword.
+  # 'a' 'b' => 'a' <ows> 'b' <ows>
+  # if ows is not added (if I want to use auto-whitespace again), this will remain the same. The single space in between will stay there.
+  # don't want to do replace in some lines, so split by line.
+  set lines [split $text "\n"]
+  set lines2 [list]
+  foreach line $lines {
+    if {[add_ows? $line]} {
+      regsub -all {('[^'']+')} $line "\\1$ows" line
+    }
+    lappend lines2 $line
+  }
+  
+  join $lines2 "\n"
+  
+}
+
+proc add_ows? {line} {
+  if {[regexp keyword $line]} {
+    #breakpoint
+  }
+  
+  foreach rule {keyword identifier floating-constant integer-constant character-constant string chars escaped-chars} {
+    if {[regexp "$rule>? ?::=" $line]} {
+      return 0
+    }
+  }
+  if {[regexp keyword $line]} {
+    #breakpoint
+  }
+  return 1
+}
+
+# [2017-01-03 21:52] Old version which does some extra processing which was useful at the start, not anymore.
+proc transform_old {text until_line} {
+  global keywords OWS
+  if {$OWS} {
+    set ows " <ows>"
+  } else {
+    set ows ""
+  }
+  # breakpoint
+  # don't include |, is used in BNF itself.
+  foreach chars {, : * ? \{ \} || << >> ^ / % ++ -- -> = == > < >= <= + - . ~ ! *= /= += -= <<= >>= ^= != |= %= ... ; ( ) [ ]} {
+    # regsub -all " \\$char " $text " '$char' " text
+    regsub -all [to_regexp $chars] $text " '$chars'$ows\\1" text
+  }
+  # & and && have special syntax in regsub, so specific replace.
+  regsub -all { \&([ \n])} $text " '\\&'$ows\\1" text
+  regsub -all { \&\&([ \n])} $text " '\\&\\&'$ows\\1" text
+  regsub -all { \&=([ \n])} $text " '\\&='$ows\\1" text
+  foreach keyword $keywords {
+    regsub -all " ${keyword}(\[ \\n\])" $text " '$keyword'$ows\\1" text
+  }
+
   # keep text until until_line, replace rest with trivial LHS ::= 'LHS'
   set lines [split $text "\n"]
   set keep_lines [lrange $lines 0 $until_line]
@@ -46,6 +111,8 @@ proc transform {text until_line} {
     }
   }
 
+  # Add dummy rules when they only appear on RHS, not LHS.
+  # [2017-01-03 21:50] Not used anymore.
   set lhs_rules [find_lhs_rules $all_lines]
   set rhs_rules [find_rhs_rules $all_lines]
   set new_rules [::struct::set difference $rhs_rules $lhs_rules]
@@ -62,6 +129,7 @@ proc transform {text until_line} {
   
   # return $text
 }
+
 
 proc keyword_def {} {
   global keywords
