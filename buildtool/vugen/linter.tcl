227ba346b0f3eb9e5d6d3933d188a610b67420fd
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2017-04-03 10:53:15 +0200

    ignore files, todo actions, ...

--- buildtool/vugen/linter.tcl
+++ buildtool/vugen/linter.tcl
@@ -1,9 +1,21 @@
 task linter {Check source files like a linter.
 } {
-  # TODO: alle .c files (en .h evt ook.)
+  # TODO: remove both /* */ and // comments. Should already have code for this.
   foreach filename [get_source_files] {
-    lint_file $filename
+    if {![lint_ignore $filename]} {
+      lint_file $filename  
+    }
   }
+}
+
+proc lint_ignore {filename} {
+  set ignores {vugen.h y_core.c}
+  foreach re $ignores {
+    if {[regexp $re $filename]} {
+      return 1
+    }
+  }
+  return 0
 }
 
 proc lint_file {filename} {
@@ -15,17 +27,25 @@ proc lint_file {filename} {
   # breakpoint
   # check var initialise can be done by checking each statement. Other things like free/= NULL combinations require checking multiple statements.
   # for free/null, maybe a macro can be used. Then the check just needs to be that free() is not used directly anymore.
+  # TODO: option to ignore certain warnings: both by type and specific:
+  # TODO: specific warnings: mark all remaining warnings in a file: both file/linenr and actual text. As long as both location and text stay the same, don't show the warning.
+  # TODO: check for no_* variables, should keep it positive.
+  # TODO: something with responsibility for free-ing vars: either caller or callee. Something with a unique-marker? (unique => caller resp, shared => callee responsibility)
   foreach stmt $statements {
     set lines [stmt_lines $stmt]
-    if {[regexp {^([^=\(\)]+);} $lines z line]} {
-      if {[regexp {break|return|continue|\+\+} $line]} {
+    if {[regexp {^([^=\(\)\{\}]+);} $lines z line]} {
+      if {[regexp {break|return|continue|\+\+|struct} $line]} {
         # continue
       } else {
+        # [2017-03-31 12:58:22] mark as 'possible' because we can give false positives, eg within structs.
         stmt_warn "Possible var declaration without assignment" $filename $stmt
       }
     }
     if {[regexp {\mfree\M} $lines]} {
-      stmt_warn "Use of free, use rb_free" $filename $stmt
+      stmt_warn "Use of free, should use rb_free" $filename $stmt
+    }
+    if {[regexp {\m[mc]alloc\M} $lines]} {
+      stmt_warn "Use of calloc/malloc, should use y_array_alloc (y_core.c)" $filename $stmt
     }
   }
 }
