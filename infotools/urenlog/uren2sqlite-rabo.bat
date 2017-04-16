a040ede22a9035626d48dc862dc1a06e09b303a2
Nico de Vreeze <nico.de.vreeze@xs4all.nl>
2017-04-16 11:40:47 +0200

    Move urenlog files to raboprj

--- infotools/urenlog/uren2sqlite-rabo.bat
+++ infotools/urenlog/uren2sqlite-rabo.bat
@@ -1,2 +1,2 @@
-tclsh uren2sqlite.tcl c:\PCC\nico\dropbox\ymor\urenlog\uren-saldo.xlsm
-
+rem tclsh uren2sqlite.tcl c:\PCC\nico\dropbox\ymor\urenlog\uren-saldo.xlsm
+tclsh uren2sqlite.tcl c:\PCC\nico\raboprj\urenlog\uren-saldo.xlsm
