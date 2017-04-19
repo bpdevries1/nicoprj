6b3c37513a4fad9b43821ce0088a5bc69fa58a24
vreeze42 <nico@ndv.nl>
2017-04-19 21:15:29 +0200

    Move subdirs

--- mediatools/db/cassette-ddl.sql
+++
@@ -1,8 +0,0 @@
-create table cassette (
-  id integer,
-  bandnr varchar(10),
-  titel varchar(255),
-  artiest varchar(255),
-  notes varchar(255)
-);
-
--- mediatools/rpi/install-pi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# scp *.tcl pi@pi:/home/pi/bin
-# cp *.tcl /media/nas5tb_1/home-pi/bin
-unison -auto -batch rpi-bin
-
