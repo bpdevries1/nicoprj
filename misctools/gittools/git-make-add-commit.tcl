f229e3714220499c3cfa6d8236f11bea67952624
vreeze42 <nico@ndv.nl>
2017-04-27 23:19:36 +0200

    Allow eg 'nicoprj' as first cmdline param

--- misctools/gittools/git-make-add-commit.tcl
+++ misctools/gittools/git-make-add-commit.tcl
@@ -5,42 +5,35 @@ package require ndv
 # history
 # 2015-08-11 NdV ook delete uit status halen en git rm maken.
 
+use libfp
+
 set_log_global info {filename -}
 
 proc main {argv} {
-  # eerst alleen nicoprj
   set os [det_os]
   if {$os == "windows"} {
-    # set git_exe "c:/util/GitHub/cmd/git.exe"
     set git_exe [which_git c:/util/GitHub/cmd c:/PCC/util/cygwin/bin]
   } else {
     set git_exe "git"
   }
-  set commit_msg ""
-  if {[:# $argv] > 0} {
-    if {[:# $argv] == 1} {
-      lassign $argv commit_msg
-      set dir "."
-    } else {
-      lassign $argv dir commit_msg      
-    }
-    cd $dir
-  } else {
-    set commit_msg check
-    set dir .
-  }
+  lassign [det_dir_commit_msg $argv] dir commit_msg
+  set orig_dir [pwd]
+  cd $dir
   # set res [exec git status]
   set res [exec $git_exe status]
   puts "result of git-status:"
   puts $res
-  set filename "git-add-commit.sh" 
+  # put git-add-commit.sh in current working dir where gac was called from.
+  set filename [file join $orig_dir "git-add-commit.sh"]
   set f [open $filename w]
   fconfigure $f -translation lf
   puts $f "# $filename"
+  puts $f "cd $dir"
   puts $f "# Adding files to git and commit"
   set has_changes [puts_changes $f $res $commit_msg]
   puts $f "# remove file after executing"
   puts $f "rm $filename"
+  puts $f "cd -"  
   puts $f "# name of file to exec: $filename"
   close $f
   if {$os == "windows"} {
@@ -51,10 +44,50 @@ proc main {argv} {
     exec -ignorestderr chmod +x $filename
     exec -ignorestderr gedit $filename &
   }
+  cd $orig_dir
 }
 
+# [2017-04-27 22:49] commit msg meegeven doe ik eigenlijk nooit meer. Dir ook niet,
+# maar nu nico functie door bv 'gac nicoprj' te kunnen zeggen, dan tijdelijke cd naar deze
+# dir en hier git status etc uitvoeren.
+proc det_dir_commit_msg {argv} {
+  set dir "."
+  set commit_msg "check"
+  if {[count $argv] > 0} {
+    if {[count $argv] == 1} {
+      #lassign $argv commit_msg
+      #set dir "."
+      set dir [find_dir [first $argv]]
+      if {$dir == ""} {
+        log warn "dir not found: $argv, using ."
+        set dir "."
+      }
+      set commit_msg "check"
+    } else {
+      # [2017-04-27 22:51] Deze nog als fallback houden.
+      lassign $argv dir commit_msg
+    }
+  } else {
+    set commit_msg check
+    set dir .
+  }
+  list $dir $commit_msg
+}
+
+# search git-dir in default locations
+proc find_dir {dir} {
+  set root_dirs [list "~" "c:/PCC/Nico" "c:/Nico"]
+  foreach root $root_dirs {
+    set path [file join $root $dir]
+    if {[file exists $path]} {
+      return [file normalize $path]
+    }
+  }
+  return ""
+}
+
+# TODO: deze dingen uit configs lezen?
 proc det_npp_exe {} {
-  # set lst_loc {"c:/util/notepad++/notepad++.exe" "H:\\Disciplines\\Trim\\Testing\\Tooling\\Notepad++Portable\\Notepad++Portable.exe"}
   set lst_loc {"c:/util/notepad++/notepad++.exe" "C:\\PCC\\Util\\PortableApps\\Notepad++Portable\\app\\notepad++\\notepad++.exe"}
   foreach loc $lst_loc {
     if {[file exists $loc]} {
@@ -127,7 +160,7 @@ proc puts_changes {f res commit_msg} {
     }
   }
   set prev_dir "<none>"
-  set dt [clock format [clock seconds] -format "%Y-%m-%d %H:%M:%S"]
+  # set dt [clock format [clock seconds] -format "%Y-%m-%d %H:%M:%S"]
   foreach file [lsort $files] {
     set dir [file dirname $file]
     if {$dir != $prev_dir} {
